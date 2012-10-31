3768b7e39131721b20a9e849a1e4e811896788bd
ks07 <gfield145@gmail.com>
2012-10-31 01:27:31 +0000

    Remove debugging statements.

--- COMS11300/CWK3/power_4.c
+++ COMS11300/CWK3/power_4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>
 
-#define STACK_MAX_LENGTH 5
+#define STACK_MAX_LENGTH 50
 
 /* Raises any real number x to the power y, where y is an integer. */
 double power(double x, int y) {
@@ -16,16 +15,12 @@ double power(double x, int y) {
   if (y < 0) {
     for (; y < -1; y++) {
       result = result * x;
-      /* Debug Code
-	 printf("x: %f, y: %d, r: %f\n", x, y, result); */
     }
 
     result = 1.0 / result;
   } else {
     for (; y > 1; y--) {
       result = result * x;
-      /* Debug Code
-	 printf("x: %f, y: %d, r: %f\n", x, y, result); */
     }
   }
 
@@ -68,11 +63,6 @@ double powerLoop(const double x, const int y) {
   stackY[sCurrLen] = y;
 
   while (loopState != 0) {
-    /* DEBUG */
-    /* printf("State: %d CurrLen: %d Result: %f CurrStack: %d\n", loopState, sCurrLen, result, stackY[sCurrLen]); */
-    /* sleep(1); */
-
-
     if (stackY[sCurrLen] == 0) {
       /* Should only be the case when going down. Turning point. */
       result = 1;
