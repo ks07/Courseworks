cf5eb2f6be24247ce6c36d8590f1a00c8fe08e86
ks07 <gfield145@gmail.com>
2012-11-08 11:53:13 +0000

    Begin replacing XOR gates.

--- COMS12700/appx_icarus/fa.v
+++ COMS12700/appx_icarus/fa.v
@@ -4,9 +4,11 @@ module fa( output wire co,
 	   input wire x,
 	   input wire y );
 
-   wire 	      w[ 2 : 0 ];
+   wire 	      w[ 5 : 0 ];
 
-   xor t2( s, w[0], ci );
+   //xor t2( s, w[0], ci );
+   or t2( w[3], w[0], ci );
+   and t5( w[4], w[0], ci
    and t1( w[1], x, y );
    
    xor t0( w[0], x, y );
@@ -14,4 +16,14 @@ module fa( output wire co,
 
    or t4( co, w[1], w[2] );
 
-endmodule
+	   endmodule // fa
+
+	   module altXOR(input wire w0,
+			 input wire w1,
+			 output wire w2);
+	   
+	   wire w[ 5 : 0 ];
+	   
+	   
+	   endmodule // altXOR
+	   
