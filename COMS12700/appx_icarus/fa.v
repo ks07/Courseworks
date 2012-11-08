9610845dab8a9f9b56f8f586d58bd42126cdf55d
ks07 <gfield145@gmail.com>
2012-11-08 11:41:24 +0000

    Change module to use a single 3-element wire vector.

--- COMS12700/appx_icarus/fa.v
+++ COMS12700/appx_icarus/fa.v
@@ -4,14 +4,14 @@ module fa( output wire co,
 	   input wire x,
 	   input wire y );
 
-   wire 	      w0, w1, w2;
+   wire 	      w[ 2 : 0 ];
 
-   xor t2( s, w0, ci );
-   and t1( w1, x, y );
+   xor t2( s, w[0], ci );
+   and t1( w[1], x, y );
    
-   xor t0( w0, x, y );
-   and t3( w2, w0, ci );
+   xor t0( w[0], x, y );
+   and t3( w[2], w[0], ci );
 
-   or t4( co, w1, w2 );
+   or t4( co, w[1], w[2] );
 
 endmodule
