94ea3ed5c647a795d0f7976f41b24d193987cb12
ks07 <gfield145@gmail.com>
2012-11-13 14:51:58 +0000

    Complete condenseName function.

--- COMS11300/CWK4/soundex.c
+++ COMS11300/CWK4/soundex.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
-char[] condenseName(char[] name, int length) {
-  int newLength = length;
+/* Removes duplicate characters from a string. */
+char *condenseName(char name[], int length) {
+  int newLength = length + 1;
+  int i;
 
-  for (int i = 0; i < length ; i++) {
+  for (i = 0; i < (length - 1) ; i++) {
     if (name[i] == name[i + 1]) {
       name[i + 1] = '\0';
       i++;
@@ -11,8 +14,23 @@ char[] condenseName(char[] name, int length) {
     }
   }
 
-  char[newLength] ret;
-  for (int i = 0; i < length ; i++) {
-    
-  
+  int j = 0;
+
+  char ret[newLength];
+  for (i = 0; i < length ; i++) {
+    if (name[i] != '\0') {
+      ret[j] = name[i];
+      j++;
+    }  
+  }
+  ret[newLength - 1] = '\0';
+  return strdup(ret);
+}
+
+int main(void) {
+  char s[26];
+  strncpy(s, "ababbccddeeffgghhiijjkkllmmm", 26);
+  printf("%s\n", s);
+  printf("%s\n", condenseName(s, 26));
+  return 0;
 }
