d7e468802c22ddf34b9d19dc8faf686b18ddac56
ks07 <gfield145@gmail.com>
2012-11-14 01:03:36 +0000

    Finish first draft of soundex.

--- COMS11300/CWK4/soundex.c
+++ COMS11300/CWK4/soundex.c
@@ -23,14 +23,14 @@ char *condenseName(char name[], int length) {
       j++;
     }  
   }
-  ret[newLength - 1] = '\0';
+  ret[j] = '\0';
   return strdup(ret);
 }
 
 /* Removes all vowels from a string after the first letter. */
 char *stripVowels(char name[], int length) {
   int newLength = length + 1;
-  int i;
+  int i, j;
   for (i = 0; i < length; i++) {
     switch (name[i]) {
     case 'a':
@@ -43,12 +43,95 @@ char *stripVowels(char name[], int length) {
       break;
     }
   }
+
+  char ret[newLength];
+  j = 0;
+  for (i = 0; i < length; i++) {
+    if (name[i] != '\0') {
+      ret[j] = name[i];
+      j++;
+    }
+  }
+
+  ret[j] = '\0';
+  return strdup(ret);
 }
 
+char toUpper(char letter) {
+  if (letter > 96 && letter < 123) {
+    /* In ASCII encoding, lowercase letters are 32 positions ahead of their
+       uppercase counterparts. */
+    return letter - 32;
+  } else {
+    return letter;
+  }
+}
+
+char getCodeValue(char letter) {
+  switch(toUpper(letter)) {
+  case 'B':
+  case 'P':
+  case 'F':
+  case 'V':
+    return '1';
+  case 'C':
+  case 'S':
+  case 'K':
+  case 'G':
+  case 'J':
+  case 'Q':
+  case 'X':
+  case 'Z':
+    return '2';
+  case 'D':
+  case 'T':
+    return '3';
+  case 'L':
+    return '4';
+  case 'M':
+  case 'N':
+    return '5';
+  case 'R':
+    return '6';
+  default:
+    return '0';
+  }
+}
+
+char *generateSOUNDEX(char name[], int len) {
+  /* This needs to be unsigned to avoid a warning from the compiler - negative array indexes are not valid! */
+  unsigned char count = 1;
+  char encoded[5];
+  int i = 1;
+
+  encoded[0] = toUpper(name[0]);
+  encoded[1] = '0';
+  encoded[2] = '0';
+  encoded[3] = '0';
+  encoded[4] = '\0';
+
+  while (count < 4 && i < len) {
+    encoded[count] = getCodeValue(name[i]);
+    if (encoded[count] != '0') {
+      count++;
+    }
+    i++;
+  }
+
+  return strdup(encoded);
+} 
+
 int main(void) {
-  char s[26];
-  strncpy(s, "ababbccddeeffgghhiijjkkllmmm", 26);
-  printf("%s\n", s);
-  printf("%s\n", condenseName(s, 26));
+  char s[32];
+  strcpy(s, "Lem-Young");
+  char *t = strdup(s);
+  t = condenseName(t, strlen(t));
+  printf("%s\n", t);
+  t = stripVowels(t, strlen(t));
+  printf("%s\n", t);
+  t = generateSOUNDEX(t, strlen(t));
+  printf("%s\n", t);
+  //printf("%s\n", condenseName(s, 26));
+
   return 0;
 }
