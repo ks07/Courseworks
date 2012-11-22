1b7891e794c2a96c2a87ce2fc74e7d1ecc39d2ca
ks07 <gfield145@gmail.com>
2012-11-22 23:06:57 +0000

    Implement subtraction and working toggle.

--- COMS12700/Enc1/sub_8bit.v
+++ COMS12700/Enc1/sub_8bit.v
@@ -22,29 +22,37 @@ endmodule //full_adder
 // Small module to simplify neg module, less repetition.
 module neg_sub( input wire                 x,
 		input wire                 n,
+		input wire                 i,
 		output wire               ox,
 		output wire               on );
 
+   wire 				  w1, w2;
+   
+   
    xor t0( ox, x, n );
-   or t1( on, x, n );
+   or t1( w1, x, n );
+   not t2( w2, i );
+   and t3( on, w1, w2 );
    
 endmodule //neg_sub
 
 // Takes the value of i and calculates i*(-1). For use in subtraction.
 module neg( input wire signed [ 7 : 0 ]    i,
+	    input wire                     a,
 	    output wire signed [ 7 : 0 ]   o );
 
    wire 				      n[ 7 : 0 ];
-   assign n[0] = 0;
 
-   neg_sub t0( i[0], n[0], o[0], n[1] );
-   neg_sub t1( i[1], n[1], o[1], n[2] );
-   neg_sub t2( i[2], n[2], o[2], n[3] );
-   neg_sub t3( i[3], n[3], o[3], n[4] );
-   neg_sub t4( i[4], n[4], o[4], n[5] );
-   neg_sub t5( i[5], n[5], o[5], n[6] );
-   neg_sub t6( i[6], n[6], o[6], n[7] );
-   neg_sub t7( i[7], n[7], o[7], ); // Blank param => ignore output
+   assign n[0] = 0;
+   
+   neg_sub t0( i[0], n[0], a, o[0], n[1] );
+   neg_sub t1( i[1], n[1], a, o[1], n[2] );
+   neg_sub t2( i[2], n[2], a, o[2], n[3] );
+   neg_sub t3( i[3], n[3], a, o[3], n[4] );
+   neg_sub t4( i[4], n[4], a, o[4], n[5] );
+   neg_sub t5( i[5], n[5], a, o[5], n[6] );
+   neg_sub t6( i[6], n[6], a, o[6], n[7] );
+   neg_sub t7( i[7], n[7], a, o[7], ); // Blank param => ignore output
 
 endmodule //neg
 
@@ -58,7 +66,7 @@ module sub_8bit( input  wire                  op,
    wire [ 7 : 0 ] 			       c;
    wire [ 7 : 0 ] 			       b;
 
-   neg t_( y, b );
+   neg t_( y, op, b );   
 
    full_adder t0( x[0], b[0], ci, r[0], c[0] );
    full_adder t1( x[1], b[1], c[0], r[1], c[1] );
@@ -68,6 +76,5 @@ module sub_8bit( input  wire                  op,
    full_adder t5( x[5], b[5], c[4], r[5], c[5] );
    full_adder t6( x[6], b[6], c[5], r[6], c[6] );
    full_adder t7( x[7], b[7], c[6], r[7], of );
-   
 
 endmodule //sub_8bit
