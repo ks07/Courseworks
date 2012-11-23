78ac2de0719a3c8a8ccae02c309f6b977085af9c
ks07 <gfield145@gmail.com>
2012-11-23 00:41:35 +0000

    Fix behaviour of subtraction to perform x - y - ci, not x - y + ci.

--- COMS12700/Enc1/sub_8bit.v
+++ COMS12700/Enc1/sub_8bit.v
@@ -23,36 +23,41 @@ endmodule //full_adder
 module neg_sub( input wire                 x,
 		input wire                 n,
 		input wire                 i,
+		input wire                ci,
 		output wire               ox,
 		output wire               on );
 
-   wire 				  w1, w2;
+   wire 				  w1, w2, w3, w4;
    
    
    xor t0( ox, x, n );
    or t1( w1, x, n );
    not t2( w2, i );
-   and t3( on, w1, w2 );
+   and t3( w3, w1, w2 );
+   and t4( w4, w2, ci );
+   or t5( on, w4, w3 );
+   
    
 endmodule //neg_sub
 
 // Takes the value of i and calculates i*(-1). For use in subtraction.
 module neg( input wire signed [ 7 : 0 ]    i,
 	    input wire                     a,
+	    input wire                    ci,
 	    output wire signed [ 7 : 0 ]   o );
 
    wire 				      n[ 7 : 0 ];
 
    assign n[0] = 0;
    
-   neg_sub t0( i[0], n[0], a, o[0], n[1] );
-   neg_sub t1( i[1], n[1], a, o[1], n[2] );
-   neg_sub t2( i[2], n[2], a, o[2], n[3] );
-   neg_sub t3( i[3], n[3], a, o[3], n[4] );
-   neg_sub t4( i[4], n[4], a, o[4], n[5] );
-   neg_sub t5( i[5], n[5], a, o[5], n[6] );
-   neg_sub t6( i[6], n[6], a, o[6], n[7] );
-   neg_sub t7( i[7], n[7], a, o[7], ); // Blank param => ignore output
+   neg_sub t0( i[0], n[0], a, ci, o[0], n[1] );
+   neg_sub t1( i[1], n[1], a, ci, o[1], n[2] );
+   neg_sub t2( i[2], n[2], a, ci, o[2], n[3] );
+   neg_sub t3( i[3], n[3], a, ci, o[3], n[4] );
+   neg_sub t4( i[4], n[4], a, ci, o[4], n[5] );
+   neg_sub t5( i[5], n[5], a, ci, o[5], n[6] );
+   neg_sub t6( i[6], n[6], a, ci, o[6], n[7] );
+   neg_sub t7( i[7], n[7], a, ci, o[7], ); // Blank param => ignore output
 
 endmodule //neg
 
@@ -66,7 +71,7 @@ module sub_8bit( input  wire                  op,
    wire [ 7 : 0 ] 			       c;
    wire [ 7 : 0 ] 			       b;
 
-   neg t_( y, op, b );   
+   neg t_( y, op, ci, b );   
 
    full_adder t0( x[0], b[0], ci, r[0], c[0] );
    full_adder t1( x[1], b[1], c[0], r[1], c[1] );
