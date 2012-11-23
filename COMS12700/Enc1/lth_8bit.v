4e1434f97c26f78f3f394aebaa9e459e798fdd9e
ks07 <gfield145@gmail.com>
2012-11-23 12:58:10 +0000

    Template solution to 8 bit less than module.

--- COMS12700/Enc1/lth_8bit.v
+++ COMS12700/Enc1/lth_8bit.v
@@ -1,7 +1,15 @@
+// Calculates if x is less than y.
 module lth_8bit( output wire                  r,
                  input  wire signed [ 7 : 0 ] x,
                  input  wire signed [ 7 : 0 ] y );
 
-  // fill in this module with solution
+   wire 			    [ 7 : 0 ] w;
+   wire 				      o;
+   assign o = 1;
+ 				      
+   // Replace with sub_8bit module when complete.
+   assign w = x - y;
+
+   and t0( r, w[7], o );
 
 endmodule
