338e3ca16ab6ba48b429b57686e4846bad0e3f39
ks07 <gfield145@gmail.com>
2012-11-23 12:22:59 +0000

    Implement 1 bit equals.

--- COMS12700/Enc1/equ_8bit.v
+++ COMS12700/Enc1/equ_8bit.v
@@ -1,7 +1,20 @@
+module equ_1bit( output wire                  r,
+		 input  wire                  x,
+		 input  wire                  y );
+
+   wire 				      w;
+
+   xor t0( w, x, y );
+   not t1( r, w );
+
+endmodule //equ_1bit
+
 module equ_8bit( output wire                  r,
                  input  wire signed [ 7 : 0 ] x,
                  input  wire signed [ 7 : 0 ] y );
 
-  // fill in this module with solution
+   wire 				      w [ 100 : 0 ];
+
+   
 
 endmodule
