870505155b003d27f187fd57d70b138a0905ba94
ks07 <gfield145@gmail.com>
2012-11-29 10:04:56 +0000

    Add (broken) min1 gates.

--- COMS12700/Enc1/sub_8bit.v
+++ COMS12700/Enc1/sub_8bit.v
@@ -59,6 +59,42 @@ module neg( input wire signed [ 7 : 0 ]    i,
 
 endmodule //neg
 
+module min1_1bit( input wire                   x,
+		  input wire                   s,
+		  output wire                  rs,
+		  output wire                  r );
+
+   wire 				       w1, w2, w3, w4;
+
+   xor t0( r, x, s );
+   and t1( rs, x, s );
+   
+   
+endmodule //min1_1bit
+
+module min1_8bit( input wire signed [ 7 : 0 ]  x,
+		  input wire                   op,
+		  input wire                   ci,
+		  output wire signed [ 7 : 0 ] r );
+
+   wire 				       w[ 7 : 0 ];
+   wire 				       rs[ 7 : 0 ];
+   wire                                        ciop;
+
+   and t_( ciop, ci, op );
+   
+
+   min1_1bit t0( x[0], ciop, rs[1], r[0] );
+   min1_1bit t1( x[1], rs[1], rs[2], r[1] );
+   min1_1bit t2( x[2], rs[2], rs[3], r[2] );
+   min1_1bit t3( x[3], rs[3], rs[4], r[3] );
+   min1_1bit t4( x[4], rs[4], rs[5], r[4] );
+   min1_1bit t5( x[5], rs[5], rs[6], r[5] );
+   min1_1bit t6( x[6], rs[6], rs[7], r[6] );
+   min1_1bit t7( x[7], rs[7], , r[7] );
+
+endmodule //min1_8bit
+
 // An 8 bit adder and subtractor with oveflow detection.
 // If op = 1, r = x - y - ci, else r = x + y + ci
 module sub_8bit( input  wire                  op,
@@ -70,20 +106,25 @@ module sub_8bit( input  wire                  op,
 
    wire [ 7 : 0 ] 			       c;
    wire [ 7 : 0 ] 			       b;
-   wire 				       w [ 2 : 0 ];
+   wire 				       w [ 4 : 0 ];
+   wire [ 7 : 0 ] 			       pr;
+ 			       
 
    // Subtraction = x + y*(-1)
    neg t_( y, op, ci, b );   
 
    // Ripple-carry adder.
-   full_adder t0( x[0], b[0], ci, r[0], c[0] );
-   full_adder t1( x[1], b[1], c[0], r[1], c[1] );
-   full_adder t2( x[2], b[2], c[1], r[2], c[2] );
-   full_adder t3( x[3], b[3], c[2], r[3], c[3] );
-   full_adder t4( x[4], b[4], c[3], r[4], c[4] );
-   full_adder t5( x[5], b[5], c[4], r[5], c[5] );
-   full_adder t6( x[6], b[6], c[5], r[6], c[6] );
-   full_adder t7( x[7], b[7], c[6], r[7], c[7] );
+   full_adder t0( x[0], b[0], ci, pr[0], c[0] );
+   full_adder t1( x[1], b[1], c[0], pr[1], c[1] );
+   full_adder t2( x[2], b[2], c[1], pr[2], c[2] );
+   full_adder t3( x[3], b[3], c[2], pr[3], c[3] );
+   full_adder t4( x[4], b[4], c[3], pr[4], c[4] );
+   full_adder t5( x[5], b[5], c[4], pr[5], c[5] );
+   full_adder t6( x[6], b[6], c[5], pr[6], c[6] );
+   full_adder t7( x[7], b[7], c[6], pr[7], c[7] );
+
+   // CI
+   min1_8bit t7_( pr, op, ci, r );
 
    // Overflow detection.
    // If inputs (after processing by t_) are of different signs, there will be
