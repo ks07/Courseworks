a0ecd6beb956dde891e72d22a67c8617850d43fc
ks07 <gfield145@gmail.com>
2012-11-29 21:14:36 +0000

    Implement calculation of r[2] (B').

--- COMS12700/Enc1/sbox.v
+++ COMS12700/Enc1/sbox.v
@@ -1,3 +1,4 @@
+// A substitution box, implementing the PRESENT block cipher.
 module sbox( output wire [ 3 : 0 ] r,
              input  wire [ 3 : 0 ] x );
 
@@ -17,7 +18,31 @@ module sbox( output wire [ 3 : 0 ] r,
 
    or ta9( r[3], a[8], a[4] );
 
-   assign r[2] = 0;
+   wire 			   b [ 14 : 0 ];
+
+   // B'
+   not tb0( b[0], x[2] );
+   not tb1( b[1], x[1] );
+   not tb2( b[2], x[0] );
+
+   and tb3( b[3], a[0], b[0] );
+   and tb4( b[4], b[1], b[3] );
+
+   and tb5( b[5], a[0], x[2] );
+   and tb6( b[6], x[1], x[0] );
+   and tb7( b[7], b[5], b[6] );
+
+   and tb8( b[8], x[1], b[2] );
+   and tb9( b[9], b[8], b[0] );
+
+   and tb10( b[10], b[1], x[3] );
+   or tb11( b[11], x[2], x[0] );
+   and tb12( b[12], b[10], b[11] );
+   
+   or tb13( b[13], b[4], b[7] );
+   or tb14( b[14], b[9], b[12] );
+   or tb15( r[2], b[14], b[13] );
+
    assign r[1] = 0;
    assign r[0] = 0;
    
