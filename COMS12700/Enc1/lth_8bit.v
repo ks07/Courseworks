ad8dd958fa763bed9d68a50449f5b1b00088f615
ks07 <gfield145@gmail.com>
2012-11-29 12:49:16 +0000

    Implement lth_8bit using sub_8bit.

--- COMS12700/Enc1/lth_8bit.v
+++ COMS12700/Enc1/lth_8bit.v
@@ -1,15 +1,20 @@
-// Calculates if x is less than y.
+// Calculates if x is less than y. (r = 1 if x < y)
 module lth_8bit( output wire                  r,
                  input  wire signed [ 7 : 0 ] x,
                  input  wire signed [ 7 : 0 ] y );
 
    wire 			    [ 7 : 0 ] w;
-   wire 				      o;
-   assign o = 1;
+   wire 				      op;
+   wire                                       ci;
+   wire 				      of;
+   
+   assign op = 1;
+   assign ci = 0;
  				      
-   // Replace with sub_8bit module when complete.
-   assign w = x - y;
+   // Use sub_8bit module from sub_8bit.v. Verilog will automatically search files in the
+   // same directory when looking for sub_8bit.
+   sub_8bit s0( op, of, w, ci, x, y );
 
-   and t0( r, w[7], o );
-
+   xor t0( r, w[7], of );
+   
 endmodule
