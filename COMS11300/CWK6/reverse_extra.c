3532fa0cce6138ec690a7faf4d0484a390737cbb
ks07 <gfield145@gmail.com>
2012-12-03 18:07:09 +0000

    Create reverse part 2.

---
+++ COMS11300/CWK6/reverse_extra.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _charList {
+  struct _charList *next;
+  char value;
+} charList;
+
+charList *listInsert(char value, charList *list) {
+  charList *next;
+
+  next = malloc(sizeof(charList));
+  next->value = value;
+
+  if (list == NULL) {
+    list = next;
+  } else {
+    next->next = list;
+  }
+
+  return next;
+}
+
+void printList(charList *head) {
+  while (head != NULL) {
+    printf("%c", head->value);
+    head = head->next;
+  }
+
+  printf("\n");
+}
+
+int main(void) {
+  charList *head = NULL;
+  char input;
+
+  input = getchar();
+
+  while (input != '.') {
+    head = listInsert(input, head);
+    input = getchar();
+  }
+
+  head = listInsert(input, head);
+
+  printList(head);
+
+  return 0;
+}
