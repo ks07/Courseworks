5b60a568e83dc7f10f29a8d0243fbf91dfb0383e
ks07 <gfield145@gmail.com>
2012-12-04 01:20:04 +0000

    Begin extra implementation. Implement tail insert function.

--- COMS11300/CWK6/reverse_extra.c
+++ COMS11300/CWK6/reverse_extra.c
@@ -9,12 +9,10 @@ typedef struct _charList {
 charList *listInsert(char value, charList *list) {
   charList *next;
 
-  next = malloc(sizeof(charList));
+  next = calloc(1, sizeof(charList));
   next->value = value;
 
-  if (list == NULL) {
-    list = next;
-  } else {
+  if (list != NULL) {
     next->next = list;
   }
 
@@ -30,18 +28,43 @@ void printList(charList *head) {
   printf("\n");
 }
 
+/* Inserts an element at the end of a given linked list. If the list
+   provided is NULL, a new list is created. This function returns a
+   pointer to the tail of the list, i.e. the inserted element. */
+charList *listInsertTail(char value, charList *list) {
+  charList *new = calloc(1, sizeof(charList));
+  new->value = value;
+
+  if (list == NULL) {
+    list = new;
+  } else {
+    while (list->next != NULL) {
+      list = list->next;
+    }
+
+    list->next = new;
+    list = list->next;
+  }
+
+  return list;
+}
+
 int main(void) {
   charList *head = NULL;
+  charList *tail;
   char input;
 
   input = getchar();
-
-  while (input != '.') {
+  if (input != '.') {
     head = listInsert(input, head);
+    tail = head;
     input = getchar();
-  }
 
-  head = listInsert(input, head);
+    while (input != '.') {
+      tail = listInsertTail(input, tail);
+      input = getchar();
+    }
+  }
 
   printList(head);
 
