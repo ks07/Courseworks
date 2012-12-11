338babf6f09d8c4e2b5dd45869792379cd5aefbe
ks07 <gfield145@gmail.com>
2012-12-11 14:39:56 +0000

    Finish q1, import CharListRead.c

---
+++ COMS11300/E3/1.c
@@ -0,0 +1,41 @@
+/* George Field. gf12815, version C */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Will use doubles for averages, as the average is not guaranteed to be an integer.
+double average(int numbers[], int length) {
+  int i;
+  double result = 0;
+
+  for (i=0; i<length; i++) {
+    result = result + (double)numbers[i];
+  }
+
+  result = result / length;
+  return result;
+}
+
+void printDifferencesFromAvg(int numbers[], int length) {
+  double avg = average(numbers, length);
+  int i;
+
+  for (i = 0; i<length; i++) {
+    // When printing, truncate the decimal part to match with the example output.
+    printf("%.0f\n", avg - (double)numbers[i]);
+  }
+}
+
+int main(void) {
+  int numbers[40];
+  int i = 0;
+
+  do {
+    scanf("%d", &numbers[i]);
+    i++;
+  } while (i < 40 && numbers[i-1] != -1);
+
+  printDifferencesFromAvg(numbers, i);
+
+  return 0;
+}
