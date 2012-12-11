2057a3c8fe8698b38938af2ef07743cfb1cecf18
ks07 <gfield145@gmail.com>
2012-12-11 15:07:32 +0000

    Finish part 2, rename CharListRead.c

---
+++ COMS11300/E3/2_CharListRead.c
@@ -0,0 +1,65 @@
+/* George Field, gf12815, version C */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct L {
+  char     symbol;
+  struct L *next;
+} listelem;
+
+
+listelem *insert_list(char c, listelem *p) {
+  listelem *temp = calloc(1,sizeof(listelem));
+
+  temp->symbol = c;
+  temp->next = p;
+  return temp;
+}
+
+
+listelem *read_list( void ) {
+  char c;
+  listelem *lp = NULL;
+
+  c = getchar();   
+  while (c != '.') {
+    lp = insert_list(c,lp);
+    c = getchar(); 
+  }
+  return lp;
+}
+
+// Returns 1 if lists are the same, 0 otherwise.
+int compare_lists(listelem *head1, listelem *head2) {
+  int result = 1;
+
+  while ((head1 != NULL) && (head2 != NULL) && (result == 1)) {
+    if (head1->symbol != head2->symbol) {
+      result = 0;
+    } else if ((head1->next == head2->next) && (head1->next != NULL)) {
+      /* If both next pointers are the same, but head1 (and thus, head2) are not NULL,
+	 the lists differ in length. */
+      printf("\nLENGTH\n\n");
+      result = 0;
+    } else {
+      head1 = head1->next;
+      head2 = head2->next;
+    }
+  }
+
+  return result;
+}
+
+int main( void ) {
+  listelem *lp = read_list();
+  listelem *lr = read_list();
+
+  if (compare_lists(lp, lr) == 1) {
+    printf("EQUAL\n");
+  } else {
+    printf("DIFFERENT\n");
+  }
+
+  return 0;
+}
