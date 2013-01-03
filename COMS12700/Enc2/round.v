0c5b7985bcae885673609c2166a978c3130706fb
ks07 <gfield145@gmail.com>
2013-01-03 19:26:22 +0000

    Working round.v.

--- COMS12700/Enc2/round.v
+++ COMS12700/Enc2/round.v
@@ -10,9 +10,7 @@ module round( output wire [ 63 : 0 ] r,
    
    
    genvar 			     i;
-   
-   
-   
+
    key_addition ka( w, x, k );
 
    split_0 sp( split[0], split[1], split[2], split[3], split[4], split[5], split[6], split[7], split[8], split[9], split[10], split[11], split[12], split[13], split[14], split[15], w );
@@ -27,6 +25,4 @@ module round( output wire [ 63 : 0 ] r,
    merge_0 mr( m, sbd[0], sbd[1], sbd[2], sbd[3], sbd[4], sbd[5], sbd[6], sbd[7], sbd[8], sbd[9], sbd[10], sbd[11], sbd[12], sbd[13], sbd[14], sbd[15] );
 
    perm pm( r, m );
-   
-   
 endmodule
