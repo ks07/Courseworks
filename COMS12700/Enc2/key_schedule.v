287490114bb09f0c570c64684e2e89a4c3d257bf
ks07 <gfield145@gmail.com>
2013-01-04 01:21:13 +0000

    Implement left rotate.

--- COMS12700/Enc2/key_schedule.v
+++ COMS12700/Enc2/key_schedule.v
@@ -2,6 +2,16 @@ module key_schedule( output wire [ 79 : 0 ] r,
                      input  wire [ 79 : 0 ] x,
                      input  wire [  4 : 0 ] i );
 
-  // fill in this module with solution
+   wire [ 79 : 0 ] 			    rot;
+   genvar 				    j;
 
+   generate
+      for( j = 0; j < 80; j = j + 1 )
+	begin:lp1
+	   assign rot[(j + 1) % 80] = x[j];
+	end
+   endgenerate
+
+   assign r = rot;
+   
 endmodule
