289ac29ae6e8e2c8d1df2c28c79857f562c48f15
ks07 <gfield145@gmail.com>
2013-01-17 10:48:19 +0000

    Finish key_schedule.v

--- COMS12700/Enc2/key_schedule.v
+++ COMS12700/Enc2/key_schedule.v
@@ -3,15 +3,23 @@ module key_schedule( output wire [ 79 : 0 ] r,
                      input  wire [  4 : 0 ] i );
 
    wire [ 79 : 0 ] 			    rot;
+   wire [ 3 : 0 ] 			    t1;
+   
+   wire [ 4 : 0 ] 			    t2;
+   
+ 
    genvar 				    j;
 
    generate
       for( j = 0; j < 80; j = j + 1 )
 	begin:lp1
-	   assign rot[(j + 1) % 80] = x[j];
+	   assign rot[(j + 61) % 80] = x[j];
 	end
    endgenerate
 
-   assign r = rot;
+   sbox sb0(t1, rot[ 79 : 76 ]);
+   assign t2 = rot[ 19 : 15 ] ^ i;
+
+   assign r = {t1, rot[ 75 : 20 ], t2, rot[ 14 : 0 ]};
    
 endmodule
