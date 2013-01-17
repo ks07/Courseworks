cf298f70c92e49d962313de8e5cb6f7d6e0a0621
ks07 <gfield145@gmail.com>
2013-01-17 11:46:58 +0000

    Complete encrypt_v1.

--- COMS12700/Enc2/encrypt_v1.v
+++ COMS12700/Enc2/encrypt_v1.v
@@ -2,6 +2,27 @@ module encrypt_v1( input  wire [ 79 : 0 ]   K,
                    input  wire [ 63 : 0 ]   M,
                    output wire [ 63 : 0 ]   C );
 
-  // fill in this module with solution
+   genvar 				    i;
+
+   // Array of 32 64-bit wire vectors.
+   wire [ 63 : 0 ] 			    rndOut [ 30 : 0];
+   wire [ 79 : 0 ] 			    ksOut [ 30 : 0 ];
+
+   round r0( rndOut[ 0 ], M, K );
+   key_schedule ks0( ksOut[ 0 ], K, 5'b00001 );
+   
+
+   generate
+      for( i = 1; i < 31; i = i + 1 )
+	begin:lp0
+	   // Assign a localparam with which we can generate the inputs for k.
+	   localparam [ 4 : 0 ] ksCounter = 5'b00001 + i;
+	   
+	   round r( rndOut[ i ], rndOut[ i - 1 ], ksOut[ i - 1 ] );
+	   key_schedule k( ksOut[ i ], ksOut[ i - 1 ], ksCounter );
+	end
+   endgenerate
+
+   key_addition ka0( C, rndOut[ 30 ], ksOut[ 30 ] );
 
 endmodule
