6630e39fe05febb5f26febfbd656ef0389143b73
ks07 <gfield145@gmail.com>
2013-01-20 19:10:35 +0000

    Implement new hashing algorithm.

--- COMS11600/CW2/wordcount.c
+++ COMS11600/CW2/wordcount.c
@@ -137,19 +137,55 @@ typedef struct _stringOccTable {
   unsigned int bucketCount;
   unsigned int maxOverflowSize;
 } stringOccTable;
-
-// Calculate the hash of a given string key, assuring that the returned value is within the range of buckets.
+/*
+// Calculates the hash of a give string key.
 // Returns the hash value of the given key.
-unsigned int calcHash(char *key, unsigned int buckets) {
+unsigned int calculateHash(char *key) {
   char current = key[0];
-  unsigned int hash = 0;
-  int i;
+  unsigned int i, hash = 0;
 
   for (i = 1; current != '\0'; i++) {
     hash += current;
     current = key[i];
   }
 
+  return hash;
+}*/
+
+unsigned int calculateHash(char key[]) {
+  unsigned int i, j, shift, hash = 0, mod, count;
+
+  // Calculate the size of an int in terms of the size of a char.
+  const unsigned int intSize = sizeof(int) / sizeof(char);
+
+  // Calculate the number of chars in the string that do not form a complete int.
+  mod = strlen(key) % intSize;
+
+  // Calculate the limit of our loop.
+  count = (strlen(key) - mod) * 4;
+
+  // Loop through until we've constructed all possible ints.
+  for (i = 0; i < count; i = (i + 1) * 4) {
+    hash += key[i] + (key[i + 1] << 8) + (key[i + 2] << 16) + (key[i + 3] << 24);
+  }
+
+  // If we have outstanding chars, add them.
+  for (j = 0; j < mod; j++) {
+    i += j;
+    shift = j * 8;
+    hash += key[i] << shift;
+  }
+
+  return hash;
+}
+
+// Calculate the hash of a given string key, assuring that the returned value is within the range of buckets.
+// Returns the hash value of the given key.
+unsigned int calcHash(char *key, unsigned int buckets) {
+  // Use the calculateHash function to produce the raw hash.
+  unsigned int hash = calculateHash(key);
+
+  // Mod the result of the hash calculation so we don't go out of bounds.
   return hash % buckets;
 }
 
@@ -373,13 +409,14 @@ int main(int argc, char *argv[]) {
   } else {
     // Print statistics on the population process.
     printf("Time for population with %d words:\n  List: %.2f seconds Table: %.2f seconds\n", listContainer->store.list->length, clockToSeconds(listTimer), clockToSeconds(tableTimer));
+    printTableMetadata(tableContainer->store.table);
 
     // Loop, asking the user to give a word to lookup, then asking whether they would like to search again.
     do {
       printf("Enter word for retrieval: ");
       scanf("%99s", choice);
       strToLower(choice);
-
+      
       printf("List: ");
       printLookupResult(listSearch(choice, listContainer->store.list));
 
