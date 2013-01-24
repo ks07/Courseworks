fd68001d77aedfc35866b4bd154e7d87840f7a4e
ks07 <gfield145@gmail.com>
2013-01-24 11:40:50 +0000

    fix this shit.

--- COMS12700/Enc2/encrypt_v3.v
+++ COMS12700/Enc2/encrypt_v3.v
@@ -5,49 +5,48 @@ module encrypt_v3( input  wire            clk,
                    output wire [ 63 : 0 ]   C );
 
    genvar 				    i;
-
+   
+   integer 				    j;
+   
    // Array of 32 64-bit wire vectors.
-   reg [ 63 : 0 ] 			    rndOut [ 30 : 0];
-   reg [ 79 : 0 ] 			    ksOut [ 30 : 0 ];
-   wire [ 63 : 0 ] 			    rndOutWire [ 30 : 0];
-   wire [ 79 : 0 ] 			    ksOutWire [ 30 : 0 ];
+   reg [ 63 : 0 ] 			    rndOut [ 31 : 0];
+   reg [ 79 : 0 ] 			    ksOut [ 31 : 0 ];
+   wire [ 63 : 0 ] 			    rndOutWire [ 31 : 0];
+   wire [ 79 : 0 ] 			    ksOutWire [ 31 : 0 ];
 
    reg [ 63 : 0 ] 			    CReg;
    reg [ 79 : 0 ] 			    KAks;
    reg [ 63 : 0 ] 			    KArnd;
    
    generate
-      for( i = 1; i < 31; i = i + 1 )
+      for( i = 0; i < 31; i = i + 1 )
 	begin:lp0
 	   // Assign a localparam with which we can generate the inputs for k.
 	   localparam [ 4 : 0 ] ksCounter = 5'b00001 + i;
 
-	   round r( rndOutWire[ i ], rndOut[ i - 1 ], ksOut[ i - 1 ] );
-	   key_schedule k( ksOutWire[ i ], ksOut[ i - 1 ], ksCounter );
+	   round r( rndOutWire[ i ], rndOut[ i ], ksOut[ i ] );
+	   key_schedule k( ksOutWire[ i ], ksOut[ i ], ksCounter );
 	end
-   endgenerate
-
-   generate
-      for( i = 30; i > 0; i = i - 1 )
-	begin:lp1
-	   always @ ( posedge clk ) begin
-	      rndOut[ i ] = rndOutWire[ i ];
-	      ksOut[ i ] = ksOutWire[ i ];
-	   end
-	end
-   endgenerate
-
-   
+   endgenerate      
+      
    always @ ( posedge clk ) begin
-      // Accept a new input, feed inputs forward.
-      #1 rndOut[ 0 ] = M;
-      #1 ksOut[ 0 ] = K;      
+       rndOut[ 0 ] = M;
+       ksOut[ 0 ] = K;
    end
+
 
    always @ ( negedge clk ) begin
       // Output the result.
-      KArnd = rndOut[ 30 ];
-      KAks = ksOut [ 30 ];
+
+      for( j = 0; j < 31; j = j + 1 )
+	begin: ladies
+	    rndOut[ j + 1 ] = rndOutWire[ j ];
+            ksOut[ j + 1 ] = ksOutWire[ j ];
+	end
+
+
+      KArnd = rndOut[ 31 ];
+      KAks = ksOut [ 31 ];
    end
    
    key_addition ka0( C, KArnd, KAks );
