03ffc075fb9815268899ed4a61d6c52c519768eb
ks07 <gfield145@gmail.com>
2013-01-25 21:32:32 +0000

    Set sensible defaults. Select FNV-1a as our hash function.

--- COMS11600/CW2/wordcount.c
+++ COMS11600/CW2/wordcount.c
@@ -7,9 +7,9 @@
 
 // Define some constants with the preprocessor. These act like simple substitutions, and are seen as literals when compiling.
 #define DEFAULT_FILENAME "a_portrait.txt"
-#define HASHTABLE_BUCKETS 1009
-#define RESIZE_FACTOR 1.33f
-#define RESIZE_LIMIT 0.8f
+#define HASHTABLE_BUCKETS 2099
+#define RESIZE_FACTOR 1.5f
+#define RESIZE_LIMIT 1.0f
 
 /* General word handling functions. */
 
@@ -299,77 +299,77 @@ typedef struct _stringOccTable {
   unsigned int itemCount;
 } stringOccTable;
 
-/*
-// Calculates the hash of a give string key.
-// Returns the hash value of the given key.
-unsigned int calculateHash(char *key) {
-  char current = key[0];
-  unsigned int i, hash = 0;
 
-  for (i = 1; current != '\0'; i++) {
-    hash += current;
-    current = key[i];
-  }
+/* // Calculates the hash of a give string key. */
+/* // Returns the hash value of the given key. */
+/* unsigned int calculateHash(char *key) { */
+/*   char current = key[0]; */
+/*   unsigned int i, hash = 0; */
 
-  return hash;
-}
-*/
-/*
-unsigned int calculateHash(char key[]) {
-  unsigned int i, j, shift, hash = 0, mod, count;
+/*   for (i = 1; current != '\0'; i++) { */
+/*     hash += current; */
+/*     current = key[i]; */
+/*   } */
 
-  // Calculate the size of an int in terms of the size of a char.
-  const unsigned int intSize = sizeof(int) / sizeof(char);
+/*   return hash; */
+/* } */
 
-  // Calculate the number of chars in the string that do not form a complete int.
-  mod = strlen(key) % intSize;
 
-  // Calculate the limit of our loop.
-  count = (strlen(key) - mod) / intSize;
+/* unsigned int calculateHash(char key[]) { */
+/*   unsigned int i, j, shift, hash = 0, mod, count; */
 
-  // Loop through until we've constructed all possible ints.
-  for (i = 0; i < count; i += 4) {
-    hash += key[i] + (key[i + 1] << 8) + (key[i + 2] << 16) + (key[i + 3] << 24);
-  }
+/*   // Calculate the size of an int in terms of the size of a char. */
+/*   const unsigned int intSize = sizeof(int) / sizeof(char); */
 
-  // If we have outstanding chars, add them.
-  for (j = 0; j < mod; j++) {
-    i += j;
-    shift = j * 8;
-    hash += key[i] << shift;
-  }
+/*   // Calculate the number of chars in the string that do not form a complete int. */
+/*   mod = strlen(key) % intSize; */
 
-  return hash;
-}*/
-/*
-// Modified Bernstein hash.
-unsigned int calculateHash(char key[]) {
-  unsigned int i, hash = 0;
+/*   // Calculate the limit of our loop. */
+/*   count = (strlen(key) - mod) / intSize; */
 
-  for (i = 0; key[i] != '\0'; i++) {
-    hash = 33 * hash ^ key[i]; // ^ = XOR
-  }
+/*   // Loop through until we've constructed all possible ints. */
+/*   for (i = 0; i < count; i += 4) { */
+/*     hash += key[i] + (key[i + 1] << 8) + (key[i + 2] << 16) + (key[i + 3] << 24); */
+/*   } */
 
-  return hash;
-}*/
-/*
-// One-at-a-Time hash
-unsigned int calculateHash(char key[]) {
-  unsigned int i, hash = 0;
+/*   // If we have outstanding chars, add them. */
+/*   for (j = 0; j < mod; j++) { */
+/*     i += j; */
+/*     shift = j * 8; */
+/*     hash += key[i] << shift; */
+/*   } */
 
-  for (i = 0; key[i] != '\0'; i++) {
-    hash += key[i];
-    hash += (hash << 10);
-    hash ^= (hash >> 6);
-  }
+/*   return hash; */
+/* } */
 
-  hash += (hash << 3);
-  hash ^= (hash >> 11);
-  hash += (hash << 15);
+/* // Modified Bernstein hash. */
+/* unsigned int calculateHash(char key[]) { */
+/*   unsigned int i, hash = 0; */
 
-  return hash;
-  }*/
-/*
+/*   for (i = 0; key[i] != '\0'; i++) { */
+/*     hash = 33 * hash ^ key[i]; // ^ = XOR */
+/*   } */
+
+/*   return hash; */
+/* } */
+
+/* // One-at-a-Time hash */
+/* unsigned int calculateHash(char key[]) { */
+/*   unsigned int i, hash = 0; */
+
+/*   for (i = 0; key[i] != '\0'; i++) { */
+/*     hash += key[i]; */
+/*     hash += (hash << 10); */
+/*     hash ^= (hash >> 6); */
+/*   } */
+
+/*   hash += (hash << 3); */
+/*   hash ^= (hash >> 11); */
+/*   hash += (hash << 15); */
+
+/*   return hash; */
+/* } */
+
 // FNV-1a 32-bit hash
 unsigned int calculateHash(char key[]) {
   const unsigned int FNV_OFFSET_BASIS = 2166136261;
@@ -383,18 +383,18 @@ unsigned int calculateHash(char key[]) {
   }
 
   return hash;
-}*/
-
-// sdbm hash
-unsigned int calculateHash(char key[]) {
-  unsigned int i, hash = 0;
-
-  for (i = 0; key[i] != '\0'; i++) {
-    hash = key[i] + (hash << 6) + (hash << 16) - hash;
-  }
-
-  return hash;
 }
+
+/* // sdbm hash */
+/* unsigned int calculateHash(char key[]) { */
+/*   unsigned int i, hash = 0; */
+
+/*   for (i = 0; key[i] != '\0'; i++) { */
+/*     hash = key[i] + (hash << 6) + (hash << 16) - hash; */
+/*   } */
+
+/*   return hash; */
+/* } */
 
 // Calculate the hash of a given string key, assuring that the returned value is within the range of buckets.
 // Returns the hash value of the given key.
