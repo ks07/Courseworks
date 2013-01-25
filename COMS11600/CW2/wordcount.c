4def349297b7d7052461d7edf773fa9511036d01
ks07 <gfield145@gmail.com>
2013-01-25 12:34:30 +0000

    Implement 2 new hash functions.

--- COMS11600/CW2/wordcount.c
+++ COMS11600/CW2/wordcount.c
@@ -245,6 +245,7 @@ typedef struct _stringOccTable {
   unsigned int bucketCount;
   unsigned int maxOverflowSize;
 } stringOccTable;
+
 /*
 // Calculates the hash of a give string key.
 // Returns the hash value of the given key.
@@ -275,36 +276,6 @@ unsigned int calculateHash(char key[]) {
 
   // Loop through until we've constructed all possible ints.
   for (i = 0; i < count; i += 4) {
-    hash += key[i + 3] + (key[i + 2] << 8) + (key[i + 1] << 16) + (key[i] << 24);
-  }
-
-  // Initialise shift to 24 so that later chars are more significant
-  shift = 24;
-
-  // If we have outstanding chars, add them.
-  for (j = 0; j < mod; j++) {
-    i += j;
-    shift -= j * 8;
-    hash += key[i] << shift;
-  }
-
-  return hash;
-}
-*/
-unsigned int calculateHash(char key[]) {
-  unsigned int i, j, shift, hash = 0, mod, count;
-
-  // Calculate the size of an int in terms of the size of a char.
-  const unsigned int intSize = sizeof(int) / sizeof(char);
-
-  // Calculate the number of chars in the string that do not form a complete int.
-  mod = strlen(key) % intSize;
-
-  // Calculate the limit of our loop.
-  count = (strlen(key) - mod) / intSize;
-
-  // Loop through until we've constructed all possible ints.
-  for (i = 0; i < count; i += 4) {
     hash += key[i] + (key[i + 1] << 8) + (key[i + 2] << 16) + (key[i + 3] << 24);
   }
 
@@ -314,6 +285,34 @@ unsigned int calculateHash(char key[]) {
     shift = j * 8;
     hash += key[i] << shift;
   }
+
+  return hash;
+}*/
+/*
+// Modified Bernstein hash.
+unsigned int calculateHash(char key[]) {
+  unsigned int i, hash = 0;
+
+  for (i = 0; key[i] != '\0'; i++) {
+    hash = 33 * hash ^ key[i]; // ^ = XOR
+  }
+
+  return hash;
+  }*/
+
+// One-at-a-Time hash
+unsigned int calculateHash(char key[]) {
+  unsigned int i, hash = 0;
+
+  for (i = 0; key[i] != '\0'; i++) {
+    hash += key[i];
+    hash += (hash << 10);
+    hash ^= (hash >> 6);
+  }
+
+  hash += (hash << 3);
+  hash ^= (hash >> 11);
+  hash += (hash << 15);
 
   return hash;
 }
