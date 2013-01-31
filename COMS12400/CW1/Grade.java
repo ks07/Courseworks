1e1703354e37aecb005e1dcf9d614901e3d1071b
ks07 <gfield145@gmail.com>
2013-01-31 13:15:37 +0000

    Complete basic task. Add error checking.

--- COMS12400/CW1/Grade.java
+++ COMS12400/CW1/Grade.java
@@ -1,8 +1,45 @@
-class Grade
-{
-    public static void main(String[] args)
-    {
-        int mark = Integer.parseInt(args[0]);
-        if (mark < 40) System.out.println("Fail");
+class Grade {
+    private int rawMark;
+
+    public static void main(String[] args) {
+	try {
+	    Grade grade = new Grade(args[0]);
+
+	    System.out.println(grade.getGradeDesc());
+	} catch (IllegalArgumentException iae) {
+	    System.err.println("Error: " + iae.getMessage());
+	}
+    }
+
+    public Grade(String mark) throws IllegalArgumentException {
+	try {
+	    rawMark = Integer.parseInt(mark);
+	} catch (NumberFormatException nfe) {
+	    throw new IllegalArgumentException("The mark given is not a number!", nfe);
+	}
+
+	if (rawMark < 0 || rawMark > 100) {
+	    throw new IllegalArgumentException("Percentage mark is outside of the acceptable range!");
+	}
+    }
+
+    public String getGradeDesc() {
+	if (rawMark < 40) {
+	    return "Fail";
+	} else if (rawMark < 50) {
+	    return "Third Class";
+	} else if (rawMark < 60) {
+	    return "Lower Second Class";
+	} else if (rawMark < 70) {
+	    return "Upper Second Class";
+	} else if (rawMark < 80) {
+	    return "First";
+	} else if (rawMark < 90) {
+	    return "Above and Beyond";
+	} else if (rawMark < 100) {
+	    return "Publishable";
+	} else {
+	    return "Perfect";
+	}
     }
 }
