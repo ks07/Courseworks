0af7b6b314b2d3d9198000aaea483c616435ec95
ks07 <gfield145@gmail.com>
2013-03-16 18:18:56 +0000

    Fix total2.

--- COMS12400/CW6/Mind.hs
+++ COMS12400/CW6/Mind.hs
@@ -12,9 +12,9 @@ valid len inp = if length inp == len then digits inp else False
 -- Finds the gold score for a given secret and guess
 gold :: String -> String -> Integer
 gold [] [] = 0
-gold (gC : guess) (sC : secret) =
-  if gC == sC then 1 + gold guess secret else gold guess secret
-gold guess secret = error "Length mismatch."
+gold (sC : secret) (gC : guess) =
+  if gC == sC then 1 + gold secret guess else gold secret guess
+gold secret guess = error "Length mismatch."
 
 -- Test whether an element is contained in a list
 contains :: (Eq a) => a -> [a] -> Bool
@@ -24,10 +24,20 @@ contains needle (next : haystack) =
 
 -- Remove the first occurence of a character from a string
 remove :: (Eq a) => a -> [a] -> [a]
-remove needle [] = error "Needle does not exist inside haystack."
+remove needle [] = error "Haystack is empty."
 remove needle haystack = remove2 needle haystack []
   where            
     remove2 :: (Eq a) => a -> [a] -> [a] -> [a]
     remove2 needle [] previous = error "Needle does not exist in haystack."
     remove2 needle (next : haystack) previous =
       if needle == next then previous ++ haystack else remove2 needle haystack (previous ++ [next])
+
+-- Find the total score for a given secret and guess
+total :: String -> String -> Integer
+total secret guess =
+  if (length secret) == (length guess) then total2 secret guess else error "Length mismatch." 
+  where
+    total2 :: String -> String -> Integer
+    total2 secret (gC : guess) =
+      if contains gC secret then 1 + total2 secret guess else total2 secret guess
+    total2 secret [] = 0
