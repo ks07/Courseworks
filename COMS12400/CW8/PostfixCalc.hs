430a3f4493632497febac9a2baa6252b835e447d
ks07 <gfield145@gmail.com>
2013-04-24 18:49:36 +0100

    Support decimal values and new operators.

--- COMS12400/CW8/PostfixCalc.hs
+++ COMS12400/CW8/PostfixCalc.hs
@@ -5,14 +5,36 @@ stringToTokens :: String -> [String]
 stringToTokens expr = words expr
 
 isOperator :: String -> Bool
-isOperator str = str == "+"
+isOperator "+" = True
+isOperator "-" = True
+isOperator "*" = True
+isOperator "/" = True
+isOperator other = False
+
+isNumeric :: String -> Bool
+isNumeric [] = False
+isNumeric tkn = isNumeric2 tkn False
+  where
+    isNumeric2 :: String -> Bool -> Bool
+    isNumeric2 tkn True = all isDigit tkn 
+    isNumeric2 (c : tkn) False = (c == '.' || isDigit c) && isNumeric2 tkn (c == '.')
+    isNumeric2 [] dp = True
 
 getOperatorArgs :: String -> Int
 getOperatorArgs "+" = 2
+getOperatorArgs "-" = 2
+getOperatorArgs "*" = 2
+getOperatorArgs "/" = 2
 getOperatorArgs op = error "Unrecognised operator."
 
+toNum :: String -> Double
+toNum num = read num :: Double
+
 doOperation :: String -> [String] -> Double
-doOperation "+" args = (read (head args) :: Double) + (read (last args) :: Double)
+doOperation "+" (arg1 : arg0 : args) = toNum arg0 + toNum arg1
+doOperation "-" (arg1 : arg0 : args) = toNum arg0 - toNum arg1
+doOperation "*" (arg1 : arg0 : args) = toNum arg0 * toNum arg1
+doOperation "/" (arg1 : arg0 : args) = toNum arg0 / toNum arg1
 
 -- input tokens -> stack -> result
 calcPostfix :: [String] -> [String] -> Double
@@ -22,7 +44,7 @@ calcPostfix [] stack =
   else
     error "Multiple values remain in stack."
 calcPostfix (next : input) stack =
-  if all isDigit next then
+  if isNumeric next then
     -- push onto stack
     calcPostfix input (next : stack) 
   else
