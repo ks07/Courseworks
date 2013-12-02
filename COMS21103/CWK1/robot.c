7d1eb0464440f04b5ddfbfe0f3d59c3a22cca7d2
George Field <gf12815@my.bristol.ac.uk>
2013-12-02 14:44:56 +0000

    Populate graph struct.

--- COMS21103/CWK1/robot.c
+++ COMS21103/CWK1/robot.c
@@ -5,12 +5,48 @@
 #define MAX_LINE_LEN 100
 #define MAX_GRAPH_DIM 20
 
-typedef struct AdjacencyList {
-  struct LinkedList *next;
-  int toX;
-  int toY;
-  int weight;
-} AdjacencyList;
+#define N 0
+#define S 1
+#define E 2
+#define W 3
+
+typedef struct Vertex {
+  bool open;
+  struct Vertex *edge[5];
+  int dist;
+} Vertex;
+
+typedef struct Graph {
+  Vertex **nodes;
+  int maxDim;
+} Graph;
+
+Vertex **callocSquare(int size) {
+  // Allocate the rows (y)
+  Vertex **square = calloc(size, sizeof(Vertex *));
+  int i;
+  for (i = 0; i < size; i++) {
+    // Add an array for each row, creating the columns (x).
+    square[i] = calloc(size, sizeof(Vertex));
+  }
+
+  return square;
+}
+
+typedef struct PointPairList {
+  struct PointPairList *next;
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+} PointPairList;
+
+Vertex * newVertex(bool open) {
+  Vertex *new = calloc(1, sizeof(Vertex));
+  new -> dist = -1;
+  new -> open = open;
+  return new;
+}
 
 void printMap(int maxDim, bool map[maxDim][maxDim]) {
   int x, y;
@@ -54,7 +90,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-
+  // Read map definitions into a temporary matrix of blocked locations.
   while (fgets(line, MAX_LINE_LEN, mapFile) != NULL) {
     printf("%s\n", line);
     int x1, y1, x2, y2;
@@ -72,13 +108,39 @@ int main(int argc, char *argv[]) {
     } else if (line[0] == 't') {
       sscanf(line, "t %5d %5d %5d %5d ", &x1, &y1, &x2, &y2);
       printf("Teleporting %d,%d to %d,%d\n", x1, y1, x2, y2);
+
+      // TODO: Support teleports
     } else {
       // Unknown line!
       printf("Warning: ignored line %s\n", line);
     }
   }
 
+  // Close the file.
   fclose(mapFile);
+
+  // Build a graph/vertex representation of the map.
+  Graph *graph = malloc(sizeof(Graph));
+  graph -> maxDim = graphLim;
+  graph -> nodes = callocSquare(graphLim);
+  for (y = 0; y < graphLim; y++) {
+    for (x = 0; x < graphLim; x++) {
+      if (map[y][x]) {
+	if (y < graphLim - 1 && map[y+1][x]) {
+	  // The node below us is not out of bounds and is free. Add it to our edges.
+	  graph -> nodes[y][x].edge[S] = &(graph -> nodes[y+1][x]);
+	  // We should also update the node below with a link back to us.
+	  graph -> nodes[y+1][x].edge[N] = &(graph -> nodes[y][x]);
+	}
+	if (x < graphLim - 1 && map[y][x+1]) {
+	  // The node east of us is not out of bounds and is free. Add it to our edges.
+	  graph -> nodes[y][x].edge[E] = &(graph -> nodes[y][x+1]);
+	  // We should also update the connected node with a link back to us.
+	  graph -> nodes[y][x+1].edge[W] = &(graph -> nodes[y][x]);
+	}
+      }
+    }
+  }
 
   printMap(graphLim, map);
   printf("Okay!");
