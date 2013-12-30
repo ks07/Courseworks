e5e5da30821243b4fe545aa395c766fcb63ae822
ks07 <gfield145@gmail.com>
2013-12-30 16:09:27 +0000

    Store Strings in memory with reserved names.

--- COMS22201/camle/Memory.java
+++ COMS22201/camle/Memory.java
@@ -3,17 +3,19 @@ import java.io.*;
 
 public class Memory {
 
-   
+    static int stringID = 0;
     static ArrayList<Byte> memory = new ArrayList<Byte>();
 
     static public int allocateString(String text)
     {
+	int id = stringID++; // Store a string with a unique identifier, per string and per byte.
+	int subId = 0;
 	int addr = memory.size();
 	int size = text.length();
 	for (int i=0; i<size; i++) {
-	    memory.add(new Byte("", text.charAt(i)));
+	    memory.add(new Byte(id, subId++, text.charAt(i)));
 	}
-	memory.add(new Byte("", 0));
+	memory.add(new Byte(id, subId++, 0));
 	return addr;
     }
 
@@ -41,8 +43,17 @@ class Byte {
     String varname;
     int contents;
 
+    // Construct a Byte that forms a string.
+    Byte(int sId, int bId, int c) {
+	varname = sId+"-"+bId; // Use a special name for strings, based on IDs.
+	contents = c;
+    }
+
     Byte(String n, int c)
     {
+	if (!n.isEmpty() && Character.isDigit(n.charAt(0))) {
+	    System.out.println("WARNING: Defined variable with reserved name ("+n+")! This shouldn't be possible!");
+	}
 	varname = n;
 	contents = c;
     }
