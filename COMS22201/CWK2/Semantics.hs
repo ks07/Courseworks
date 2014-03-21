d8ff1617f8828b778b736dc179fbadd84e4f44b6
ks07 <gfield145@gmail.com>
2014-03-21 18:00:59 +0000

    Finish up to b_val. Struggle with cond.

--- COMS22201/CWK2/Semantics.hs
+++ COMS22201/CWK2/Semantics.hs
@@ -38,12 +38,52 @@ subst_aexp (V v) sub with
     | v == sub = with
     | otherwise = (V v)
 
+-- Evaluates a given arithmetic expression in a given state
 a_val :: Aexp -> State -> Z
-a_val a b = 12
+a_val (N i) sigma = i
+a_val (V v) sigma = sigma v
+a_val (Add a b) sigma = (a_val a sigma) + (a_val b sigma)
+a_val (Mult a b) sigma = (a_val a sigma) * (a_val b sigma)
+a_val (Sub a b) sigma = (a_val a sigma) - (a_val b sigma)
 
+-- Finds the free variables in a given boolean expression
+fv_bexp :: Bexp -> [Var]
+fv_bexp TRUE = []
+fv_bexp FALSE = []
+fv_bexp (Neg a) = fv_bexp a
+fv_bexp (And a b) = (fv_bexp a) `union` (fv_bexp b)
+fv_bexp (Eq a b) = (fv_aexp a) `union` (fv_aexp b)
+fv_bexp (Le a b) = (fv_aexp a) `union` (fv_aexp b)
+
+-- Substitutes all occurences of the given var in a boolean expression with
+-- an arithmetic expression
+subst_bexp :: Bexp -> Var -> Aexp -> Bexp
+subst_bexp TRUE sub with = TRUE
+subst_bexp FALSE sub with = FALSE
+subst_bexp (Neg a) sub with = Neg (subst_bexp a sub with)
+subst_bexp (And a b) sub with = And (subst_bexp a sub with) (subst_bexp b sub with)
+subst_bexp (Eq a b) sub with = Eq (subst_aexp a sub with) (subst_aexp b sub with)
+subst_bexp (Le a b) sub with = Le (subst_aexp a sub with) (subst_aexp b sub with)
+
+-- Evaluates a given boolean expression in a given state
+b_val :: Bexp -> State -> T
+b_val TRUE sigma = True
+b_val FALSE sigma = False
+b_val (Neg a) sigma = not (b_val a sigma)
+b_val (And a b) sigma = (b_val a sigma) && (b_val b sigma)
+b_val (Eq a b) sigma = (a_val a sigma) == (a_val b sigma)
+b_val (Le a b) sigma = (a_val a sigma) <= (a_val b sigma)
+
+-- Conditional function that returns a function from a choice of two, using a
+-- function that returns a truth value given a parameter
+cond :: (a->T, a->a, a->a) -> (a->a)
+cond (a, b, c) = \xs a' -> case (a a') of True -> b
+                                          False -> c 
+
+-- With thanks to Sahaj (give him 20 marks plz)
 -- An example state for testing
-testState :: State
-testState "x" = 12
-testState "y" = 999
-testState "z" = -1
-testState a = undefined
+sigma_t :: State
+sigma_t "x" = 12
+sigma_t "y" = 999
+sigma_t "z" = -1
+sigma_t v = undefined
