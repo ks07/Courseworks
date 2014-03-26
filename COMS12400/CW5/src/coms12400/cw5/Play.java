9790d828232f468dfce55aa49529d82b46bc5534
George Field <gfield145@gmail.com>
2014-03-26 18:40:12 +0000

    Discover some relics of Java past.

--- COMS12400/CW5/src/coms12400/cw5/Play.java
+++ COMS12400/CW5/src/coms12400/cw5/Play.java
@@ -1,12 +1,39 @@
 package coms12400.cw5;
 
+import javax.swing.SwingUtilities;
+
 public class Play implements Runnable {
-    public static void main(String[] args) {
-        
-    }
 
     @Override
     public void run() {
+        Play program = new Play();
+        SwingUtilities.invokeLater(program);
+    }
+    
+    public static void main(String[] args) {
+        Minesweeper ms = new Minesweeper(10, 10, 20);
         
+        System.out.print(ms.toString());
+        System.out.println();
+        ms.uncoverPosition(new Position(0, 0));
+        System.out.print(ms.toString());
+        System.out.println();
+        ms.uncoverPosition(new Position(9, 7));
+        System.out.print(ms.toString());
+        System.out.println();
+        ms.uncoverPosition(new Position(8, 6));
+        System.out.print(ms.toString());
+        System.out.println();
+        ms.uncoverPosition(new Position(3, 2));
+        System.out.print(ms.toString());
+        System.out.println();
+        ms.uncoverPosition(new Position(4, 8));
+        System.out.print(ms.toString());
+        System.out.println();
+        ms.uncoverPosition(new Position(9, 1));
+        System.out.print(ms.toString());
+        System.out.println();
+        ms.uncoverPosition(new Position(9, 3));
+        System.out.print(ms.toString());
     }
 }
