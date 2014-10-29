457c765065f91809768437ac77cc2f831d07661b
ks07 <gfield145@gmail.com>
2014-10-29 15:51:11 +0000

    Updates to reference model.

--- COMS31700/A1/reference_model.v
+++ COMS31700/A1/reference_model.v
@@ -8,12 +8,38 @@ module calc1_reference (out_data1, out_data2, out_data3, out_data4, out_resp1, o
    input [0:31]  req1_data_in, req2_data_in, req3_data_in, req4_data_in;
    input [1:7] 	 reset;
 
+   // Use packed/unpacked arrays to make an array of vectors.
+   // http://electronics.stackexchange.com/questions/99507/
+   reg 		 req_busy     [1:4];
+   reg [0:31] 	 req_data_buf [1:4];
+   reg [0:3] 	 req_cmd_buf  [1:4];
+
+   initial
+     begin
+	// Init all busy states to 0.
+	req_busy[1] = 0;
+	req_busy[2] = 0;
+	req_busy[3] = 0;
+	req_busy[4] = 0;
+	out_data4 = 0;
+	
+     end
+   
    always
      @ (negedge c_clk) begin
-	
-	   out_data1 = req1_data_in;
-	
-	
+	//$display ("%t rb1: %d r1ci: %d\n\n", $time, req_busy[1], req1_cmd_in);
+		
+	// If port not busy and cmd coming in, start processing.
+	if (req_busy[1] == 0 && req1_cmd_in != 0)
+	  begin
+	     req_busy[1] = 1;
+	     req_cmd_buf[1] = req1_cmd_in;
+	     req_data_buf[1] = req1_data_in;
+
+	     // On the next clock edge, spit out the data in.
+	     // Use a non-blocking delayed assign.
+	     out_data1 <= #200 req_data_buf[1];
+	  end
      end
 
 endmodule // calc1_reference
