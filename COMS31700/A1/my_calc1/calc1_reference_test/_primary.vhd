ffe3f41f7e5ae9738908c1d0e3faec8d411efefb
ks07 <gfield145@gmail.com>
2014-10-29 15:53:39 +0000

    Use GitHub's ModelSim gitignore and remove unwanted files..

--- COMS31700/A1/my_calc1/calc1_reference_test/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity calc1_reference_test is
-end calc1_reference_test;
--- COMS31700/A1/my_calc1/example_calc1_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity example_calc1_tb is
-end example_calc1_tb;
