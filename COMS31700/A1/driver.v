a5e98e524a045f6a94fecb9ed7adedf472b910f9
ks07 <gfield145@gmail.com>
2014-10-29 15:50:25 +0000

    Rename tester to use as driver basis.

--- COMS31700/A1/driver.v
+++ COMS31700/A1/driver.v
@@ -10,6 +10,18 @@ module calc1_reference_test;
    reg [0:31]    req1_data_in, req2_data_in, req3_data_in, req4_data_in;
    reg [1:7] 	 reset;
 
+   // Define some constants.
+   localparam CMD_NOP = 0;
+   localparam CMD_ADD = 1;
+   localparam CMD_SUB = 2;
+   localparam CMD_LSH = 5;
+   localparam CMD_RSH = 6;
+
+   localparam RSP_NONE = 0;
+   localparam RSP_SUCC = 1;
+   localparam RSP_INOF = 2; // Invalid command or overflow
+   localparam RSP_IERR = 3;
+   
    // Instantiate a copy of the reference model named CREF
    calc1_reference CREF(out_data1, out_data2, out_data3, out_data4, out_resp1, out_resp2, out_resp3, out_resp4, c_clk, req1_cmd_in, req1_data_in, req2_cmd_in, req2_data_in, req3_cmd_in, req3_data_in, req4_cmd_in, req4_data_in, reset);
    
@@ -27,7 +39,7 @@ module calc1_reference_test;
 	// Test the data copy. Should be 0 out after 100.
 
 	# 200
-
+	  req1_cmd_in = CMD_ADD;	
 	  req1_data_in = 255;
      end
 
