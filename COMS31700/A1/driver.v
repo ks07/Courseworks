ddab35b1242f00db2fa770d6a54510c13f66fc0e
ks07 <gfield145@gmail.com>
2014-10-30 21:13:47 +0000

    Task-ify the first test, repeat across all ports to uncover bug in port 4!

--- COMS31700/A1/driver.v
+++ COMS31700/A1/driver.v
@@ -29,6 +29,22 @@ module calc1_driver(c_clk, reset, req_cmd_out[1], req_data_out[1], req_cmd_out[2
    // Drive the clock every 100ns.
    always #100 c_clk = ~c_clk;
 
+   // Task-ified tests.
+
+   // TEST 2.1.1.1
+   // 0xFFFF0000 + 0x0000FFFF
+   task TEST_2_1_1_1;
+      input integer i;
+      begin
+	 #200
+	   req_cmd_out[i] = CMD_ADD;
+	 req_data_out[i] = 32'hFFFF0000;
+	 # 200
+	   req_cmd_out[i] = CMD_NOP;
+	 req_data_out[i] = 32'h0000FFFF;
+      end
+   endtask // TEST_2_1_1_1
+   
    initial
      begin
 
@@ -38,15 +54,11 @@ module calc1_driver(c_clk, reset, req_cmd_out[1], req_data_out[1], req_cmd_out[2
 	#200
 	  reset[1] = 0;
 
-	// TEST 2.1.1.1
-	// 0xFFFF0000 + 0x0000FFFF
-	# 200
-	  req_cmd_out[1] = CMD_ADD;
-	req_data_out[1] = 32'hFFFF0000;
-
-	# 200
-	  req_cmd_out[1] = CMD_NOP;
-	req_data_out[1] = 32'h0000FFFF;
+	// Run on all 4 ports.
+	for (i = 1; i < 5; i = i + 1)
+	  begin
+	     TEST_2_1_1_1(i);
+	  end
 	
 	// # 400
 	//   req_cmd_out[2] = CMD_SUB;
