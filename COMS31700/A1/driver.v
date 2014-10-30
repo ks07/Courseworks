d2cf8591775407ff6b5910bee00c941f327ea725
ks07 <gfield145@gmail.com>
2014-10-30 23:50:53 +0000

    Add further tests to driver.

--- COMS31700/A1/driver.v
+++ COMS31700/A1/driver.v
@@ -44,7 +44,49 @@ module calc1_driver(c_clk, reset, req_cmd_out[1], req_data_out[1], req_cmd_out[2
 	 req_data_out[i] = 32'h0000FFFF;
       end
    endtask // TEST_2_1_1_1
-   
+
+   // TEST 2.1.1.2
+   // 0x0000FFFF + 0xFFFF0000
+   task TEST_2_1_1_2;
+      input integer i;
+      begin
+	 #200
+	   req_cmd_out[i] = CMD_ADD;
+	 req_data_out[i] = 32'h0000FFFF;
+	 # 200
+	   req_cmd_out[i] = CMD_NOP;
+	 req_data_out[i] = 32'hFFFF0000;
+      end
+   endtask // TEST_2_1_1_2
+
+   // TEST 2.1.1.3
+   // 0x55555555 + 0x99999999
+   task TEST_2_1_1_3;
+      input integer i;
+      begin
+	 #200
+	   req_cmd_out[i] = CMD_ADD;
+	 req_data_out[i] = 32'h55555555;
+	 # 200
+	   req_cmd_out[i] = CMD_NOP;
+	 req_data_out[i] = 32'h99999999;
+      end
+   endtask // TEST_2_1_1_3
+
+   // TEST 2.1.1.4
+   // 0x99999999 + 0x55555555
+   task TEST_2_1_1_4;
+      input integer i;
+      begin
+	 #200
+	   req_cmd_out[i] = CMD_ADD;
+	 req_data_out[i] = 32'h99999999;
+	 # 200
+	   req_cmd_out[i] = CMD_NOP;
+	 req_data_out[i] = 32'h55555555;
+      end
+   endtask // TEST_2_1_1_4
+
    initial
      begin
 
@@ -60,26 +102,23 @@ module calc1_driver(c_clk, reset, req_cmd_out[1], req_data_out[1], req_cmd_out[2
 	     TEST_2_1_1_1(i);
 	  end
 	
-	// # 400
-	//   req_cmd_out[2] = CMD_SUB;
-	// req_data_out[2] = 1;
+	// Run on all 4 ports.
+	for (i = 1; i < 5; i = i + 1)
+	  begin
+	     TEST_2_1_1_2(i);
+	  end
 	
-
-	// # 200
-	//   req_cmd_out[2] = CMD_NOP;
-	// req_data_out[2] = 100;
+	// Run on all 4 ports.
+	for (i = 1; i < 5; i = i + 1)
+	  begin
+	     TEST_2_1_1_3(i);
+	  end
 	
-	// # 100
-	//   req_cmd_out[3] = CMD_ADD;
-	// req_data_out[3] = 1;
-	// req_cmd_out[4] = CMD_ADD;
-	// req_data_out[4] = 2;
-
-	// # 200
-	//   req_cmd_out[3] = CMD_NOP;
-	// req_data_out[3] = 4;
-	// req_cmd_out[4] = CMD_NOP;
-	// req_data_out[4] = 8;
+	// Run on all 4 ports.
+	for (i = 1; i < 5; i = i + 1)
+	  begin
+	     TEST_2_1_1_4(i);
+	  end
 		
 	#800 $stop;
 	
