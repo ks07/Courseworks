68779375ddfb58f71e45c2eea6078aed5d8daf0f
George Field <gf12815@my.bristol.ac.uk>
2014-12-16 13:50:15 +0000

    Add separate run configuration to perform timing.

---
+++ COMS30004/OCL/run_timing.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+EXE="./d2q9-bgk.exe"
+CHECK="../check_results"
+EXTRA_ARGS="$MY_OCL_ARGS"
+
+print_test() {
+    if hash figlet 2>/dev/null; then
+        figlet -t "$@"
+    elif hash banner 2>/dev/null; then
+	banner "$@"
+    else
+	echo "==================="
+        echo "$@"
+	echo "==================="
+    fi
+}
+
+SHOULD_PROFILE=0
+if [ "$1" = "-profile" ]; then
+    SHOULD_PROFILE=1
+fi
+
+make oclean && make clean && make &&
+
+# Check first run, and profile if requested.
+print_test "Run 0" &&
+
+COMPUTE_PROFILE=$SHOULD_PROFILE $EXE "../cfgs/input_1024x1024.params" "../cfgs/obstacles_1024x1024_box.dat" "$EXTRA_ARGS" &&
+
+$CHECK "../refs/av_vels_1024x1024_box.dat" "../refs/final_state_1024x1024_box.dat" &&
+
+for i in {1..3}; do
+    print_test "Run $i"
+    $EXE "../cfgs/input_1024x1024.params" "../cfgs/obstacles_1024x1024_box.dat" "$EXTRA_ARGS"
+done
+
+print_test "ALL TESTS PASSED!"
