6a48da49a3af4fd5a8441c34e7a58205be57f5ad
ks07 <gfield145@gmail.com>
2015-02-10 18:54:24 +0000

    Lots of changes have been made. All kinda work but vis is broken as hell. Why me?

--- COMS30106/a1/ailp_assignment1_56626.pl
+++ COMS30106/a1/ailp_assignment1_56626.pl
@@ -12,7 +12,7 @@ q3([s,e,w,n]).
 q4a([p(3, 3), p(3, 4), p(4, 4), p(4, 3), p(4, 2), p(3, 2), p(2, 2), p(2, 3), p(2, 4), p(1, 4), p(1, 3), p(1, 2), p(1, 1), p(2, 1), p(3, 1), p(4, 1)]).
 
 %% Second deadend found is at (2,4) after backtracking to (2,3) -> (1,3)
-q4b([p(3, 3), p(3, 4), p(4, 4), p(4, 3), p(4, 2), p(3, 2), p(2, 2), p(2, 3), p(2, 4), p(1, 4), p(1, 3), p(1, 2), p(1, 4), p(2, 4)).
+q4b([p(3, 3), p(3, 4), p(4, 4), p(4, 3), p(4, 2), p(3, 2), p(2, 2), p(2, 3), p(2, 4), p(1, 4), p(1, 3), p(1, 2), p(1, 4), p(2, 4)]).
 
 %% First path == First deadend path
 q4c([p(3, 3), p(3, 4), p(4, 4), p(4, 3), p(4, 2), p(3, 2), p(2, 2), p(2, 3), p(2, 4), p(1, 4), p(1, 3), p(1, 2), p(1, 1), p(2, 1), p(3, 1), p(4, 1)]).
@@ -20,3 +20,27 @@ q4c([p(3, 3), p(3, 4), p(4, 4), p(4, 3), p(4, 2), p(3, 2), p(2, 2), p(2, 3), p(2
 %% Second path has a lot of backtracking
 q4d([p(3, 3), p(3, 4), p(4, 4), p(4, 3), p(4, 2), p(4, 1), p(3, 1), p(3, 2), p(2, 2), p(2, 3), p(2, 4), p(1, 4), p(1, 3), p(1, 2), p(1, 1), p(2, 1)]).
 
+%% q5_corner_move :-
+%% 	ailp_show_move(p(0, 0), p(0, 4)),
+%% 	ailp_show_move(p(0, 4), p(4, 4)),
+%% 	ailp_show_move(p(4, 4), p(4, 0)),
+%% 	ailp_show_move(p(4, 0), p(0, 0)).
+
+q5_corner_move :-
+	q5_corner_move_corners(C),
+	q5_corner_move_step([C]).
+
+q5_corner_move_corners(p(0, 0)).
+q5_corner_move_corners(p(0, 4)).
+q5_corner_move_corners(p(4, 4)).
+q5_corner_move_corners(p(4, 0)).
+
+q5_corner_move_step(Path) :-
+	length(Path, 4).
+q5_corner_move_step(Path) :-
+	q5_corner_move_corners(C),
+	\+ memberchk(C, Path),
+	Path = [P1|_],
+	write(P1), nl,
+	ailp_show_move(P1, C),
+	q5_corner_move_step([C|Path]).
