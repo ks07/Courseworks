3ebd2f02e82c2edb8f2598ef8f2e1d9170368254
ks07 <gfield145@gmail.com>
2015-02-11 18:43:37 +0000

    Comment and minor fixes.

--- COMS30106/a1/ailp_assignment1_56626.pl
+++ COMS30106/a1/ailp_assignment1_56626.pl
@@ -1,11 +1,15 @@
 candidate_number(56626).
 
-q1(assignment1_module:start_position_personal(P)).
+%% Get the start position.
+q1(ailp_start_position(P)).
 
+%% Calculate new position moving East from 1,1
 q2a(new_pos(p(1,1),e,P)).
 
+%% Failing line for moving North from 1,1
 q2b(19).
 
+%% Order/priority of directions.
 q3([s,e,w,n]).
 
 %% First deadend found is in fact the final cell
@@ -20,58 +24,68 @@ q4c([p(3, 3), p(3, 4), p(4, 4), p(4, 3), p(4, 2), p(3, 2), p(2, 2), p(2, 3), p(2
 %% Second path has a lot of backtracking
 q4d([p(3, 3), p(3, 4), p(4, 4), p(4, 3), p(4, 2), p(4, 1), p(3, 1), p(3, 2), p(2, 2), p(2, 3), p(2, 4), p(1, 4), p(1, 3), p(1, 2), p(1, 1), p(2, 1)]).
 
-%% Dumb hard-coded solution
-%% q5_corner_move :-
-%% 	ailp_start_position(S),
-%% 	ailp_show_move(S, p(1, 1)),
-%% 	ailp_show_move(p(1, 1), p(1, 4)),
-%% 	ailp_show_move(p(1, 4), p(4, 4)),
-%% 	ailp_show_move(p(4, 4), p(4, 1)),
-%% 	ailp_show_move(p(4, 1), p(1, 1)).
-
-%% Dumb over-engineered solution
+%% Move to all 4 corners on fixed 4x4 grid.
 q5_corner_move :-
+	%% Force a reset at the very beginning so we look nicer.
+	reset,
+	%% Grab our start position
 	ailp_start_position(S),
-	term_to_atom([S],PathA),
-	do_command([mower,console,PathA],_),
-	q5_corner_move_step(S,[S]).
+	%% Start recursive calls
+	q5_corner_move_step(S,[]).
 
+%% Define all 4 fixed corners.
 q5_corner_move_corners(p(1, 1)).
 q5_corner_move_corners(p(1, 4)).
 q5_corner_move_corners(p(4, 4)).
 q5_corner_move_corners(p(4, 1)).
 
+%% Base case for recursion.
 q5_corner_move_step(_, Path) :-
 	length(Path, 5).
+%% Recursive step.
 q5_corner_move_step(Pos, Path) :-
+	%% Find a corner to visit
 	q5_corner_move_corners(C),
+	%% Check we haven't already visited this corner
 	\+ memberchk(C, Path),
+	%% Visualise movement from current pos to new corner
 	ailp_show_move(Pos, C),
 	term_to_atom([C|Path],PathA),
 	do_command([mower,console,PathA],_),
+	%% Recurse
 	q5_corner_move_step(C, [C|Path]).
 
+%% Move to all 4 corners on any sized grid.
 q5_corner_move2 :-
+	%% Force a reset at the very beginning so we look nicer.
+	reset,
+	%% Get our start position
 	ailp_start_position(S),
-	term_to_atom([S],PathA),
-	do_command([mower,console,PathA],_),
-	q5_corner_move_step2(S,[S]).
+	%% Call recursive predicate
+	q5_corner_move_step2(S,[]).
 
+%% Get a corner.
 q5_corner_move_corners2(p(X, Y)) :-
 	ailp_grid_size(S),
 	(Y = S; Y = 1),
 	(X = S; X = 1).
 
+%% Base case for recursion.
 q5_corner_move_step2(_, Path) :-
-	length(Path, 5).
-%% Alternatively:  findall(C, q5_corner_move_corners2(C), B), subset(B, path).
+	length(Path, 4).
+%% Alternatively could check the path holds all 4 corners: findall(C, q5_corner_move_corners2(C), B), subset(B, path).
 
+%% Recursive call to move between corners.
 q5_corner_move_step2(Pos, Path) :-
+	%% Get a corner.
 	q5_corner_move_corners2(C),
+	%% Check it is unvisited
 	\+ memberchk(C, Path),
+	%% Visualise move from current pos to corner
 	ailp_show_move(Pos, C),
 	term_to_atom([C|Path],PathA),
 	do_command([mower,console,PathA],_),
+	%% Recurse
 	q5_corner_move_step2(C, [C|Path]).
 
 %% Spiral around the board starting from a corner.
@@ -79,6 +93,7 @@ q6_spiral(Path) :-
 	%% Select a start corner/rotation pair
 	q5_corner_move_corners(Pos),
 	q6_rot(Rot),
+	%% Find a suitable start direction.
 	q6_start_direction(Rot, Pos, Dir),
 	%% Force a reset so that once done backtracking the new spiral is clear. (Optional!)
 	reset,
