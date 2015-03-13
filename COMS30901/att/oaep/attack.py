b630c2a0e135a68a6fa3edede3b09efd9d0a2586
ks07 <gfield145@gmail.com>
2015-03-13 23:00:36 +0000

    Try and implement OAEP attack, but broken. Ignore pyc.

--- COMS30901/att/oaep/attack.py
+++ COMS30901/att/oaep/attack.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 from __future__ import print_function
-import sys, subprocess
+import sys, subprocess, math
 
 def get_params(paramfile) :
   infile = open(paramfile, 'r')
@@ -11,7 +11,19 @@ def get_params(paramfile) :
 
   return (N, e, ct)
 
+def N():
+  return params[0]
+
+def e():
+  return params[1]
+
+def ct():
+  return params[2]
+
 def interact(c) :
+  global queries
+  queries = queries + 1
+
   # Send ciphertext c to attack target as hex. TODO: more mystery surrounding octet strings, yet apparently this is fine
   target_in.write( "%x\n" % ( c ) )
   target_in.flush()
@@ -19,36 +31,122 @@ def interact(c) :
   # Receive result code r from attack target.
   r = int( target_out.readline().strip() )
 
-  return r
+  # Interpret result code as B comparison
+  interp = (r == 1)
+
+  if (r != 1 and r != 0):
+    print("Error code %d!\n" % r)
+
+  return (r, interp)
+
+def ceildiv(a, b):
+  # Upside down floor division!
+  return -(-a // b)
 
 def attack() :
-  # Send the ciphertext we've been given.
-  c = params[2]
-  print(c)
+  k = int(math.log(N(), 256))
+  B = 2 ** (8*(k-1))
 
-  # ... then interact with the attack target.
-  r = interact( c )
+  # Implement attack from https://www.iacr.org/archive/crypto2001/21390229.pdf
+  # 1.1
+  f1 = 2
 
-  # Print all of the inputs and outputs.
-  print("c = %s" % ( c ))
-  print("r = %d" % ( r ))
+  # 1.2
+  to_query = pow(f1, e(), N())
+  to_query = (to_query * ct()) % N()
+  (r, gte_B) = interact(to_query)
+
+  # 1.3a
+  while (not gte_B):
+    f1 = f1 * 2
+    
+    # 1.2
+    to_query = pow(f1, e(), N())
+    to_query = (to_query * ct()) % N()
+    (r, gte_B) = interact(to_query)
+
+  #1.3b
+  # Move to step 2
+  
+  #2.1
+  f2 = ( (N() + B) // B ) * f1 // 2
+  print(f2)
+
+  #2.2
+  to_query = pow(f2, e(), N())
+  to_query = (to_query * ct()) % N()
+  (r, gte_B) = interact(to_query)
+  
+  #2.3a
+  while (gte_B):
+    f2 = f2 + f1 // 2
+
+    #2.2
+    to_query = pow(f2, e(), N())
+    to_query = (to_query * ct()) % N()
+    (r, gte_B) = interact(to_query)
+
+  #2.3b
+  # Move to step 3
+
+  #3.1
+  m_min = ceildiv(N(), f2)
+  m_max = (N() + B) // f2
+
+  #3.2
+  f_tmp = (2 * B) // (m_max - m_min)
+
+  #3.3
+  i = (f_tmp * m_min) // N()
+
+  #3.4
+  f3 = ceildiv( (i * N()), m_min)
+  to_query = pow(f3, e(), N())
+  to_query = (to_query * ct()) % N()
+  (r, gte_B) = interact(to_query)
+  
+  #3.5
+  while (m_max - m_min > 1):
+    #3.5a
+    if (gte_B):
+      m_min = ceildiv( (i * N()) + B, f3 )
+    #3.5b
+    else:
+      m_max = ( (i * N()) + B ) // f3
+
+    #3.2
+    f_tmp = (2 * B) // (m_max - m_min)
+    #3.3
+    i = (f_tmp * m_min) // N()
+     #3.4
+    f3 = ceildiv( (i * N()), m_min)
+    to_query = pow(f3, e(), N())
+    to_query = (to_query * ct()) % N()
+    (r, gte_B) = interact(to_query)
+  print("f3\t%d\nB\t%d" % (f3, B))
+  print("m\t%d" % m_min)
 
 if ( __name__ == "__main__" ) :
   if (len(sys.argv) != 3) :
     print("Usage: attack.py <target executable> <parameter file>")
-    sys.exit()
+    sys.exit(1)
 
   # Read param file
   params = get_params(sys.argv[2])
 
   # Produce a sub-process representing the attack target.
   target = subprocess.Popen( args   = sys.argv[ 1 ],
-                             stdout = subprocess.PIPE, 
+                             stdout = subprocess.PIPE,
                              stdin  = subprocess.PIPE )
 
   # Construct handles to attack target standard input and output.
   target_out = target.stdout
   target_in  = target.stdin
 
+  # Set a global counter of interactions
+  queries = 0
+
   # Execute a function representing the attacker.
   attack()
+
+  print(queries)
