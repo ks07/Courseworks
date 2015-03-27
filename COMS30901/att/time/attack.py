3ea1be75a127157dd63ca081b1756c82e412339f
George Field <gfield145@gmail.com>
2015-03-27 23:48:29 +0000

    Ready att1 part 1 for submission.

--- COMS30901/att/time/attack.py
+++ COMS30901/att/time/attack.py
@@ -23,14 +23,67 @@ def interact(c):
 
   # Receive time from attack target.
   delta = int( target_out.readline().strip() )
-  m_raw = target_out.readline().strip()
-  m = int(m_raw, 16)
+  msg   = int( target_out.readline().strip(), 16 )
 
-  return {'time':delta, 'm':m, 'm_raw':m_raw}
+  return { 'time': delta, 'm': msg }
 
+def add_challenges(count, c_list, time_list, mont_c_list, mont_tmp_list, d, mont_params):
+  assert(len(time_list) == len(c_list) and len(c_list) == len(mont_c_list))
+
+  # It is absolutely crucial we perform this step in the right place with the right list of tmps.
+  # Failure to do so will ruin the attack!
+  assert check_cpow(c_list[0], d, mont_tmp_list[0], mont_params)
+
+  N = mont_params['N']
+  for i in xrange(count):
+    c = random.randrange(N)
+#    c = c_list[0]
+    from_target = interact(c)
+
+    # Fill the base lists
+    c_list.append(c)
+    time_list.append(from_target['time'])
+    mont_c_list.append(get_mont_rep(c, mont_params))
+
+    # We need to bring these values up to speed with the rest of the algo
+    c_pow_d = pow(c, d, N)
+    mont_tmp_list.append(get_mont_rep(c_pow_d, mont_params))
+
+#    assert(mont_tmp_list[-1] == mont_tmp_list[0])
+
+def check_cpow(c, d, c_, mp):
+  c_pow_d = pow(c, d, mp['N'])
+  cm = get_mont_rep(c_pow_d, mp)
+  # if cm == c_:
+  #   print("MATCH!")
+  # else:
+  #   print("NOPE...")
+  return cm == c_
+
+def update_averages(time, red1, red2, f_sum, f_cnt):
+  # Add to F1 (0) if red1, else F2 (1)
+  t = 0 if red1 else 1
+  f_sum[t] += time
+  f_cnt[t] += 1
+  
+  # Add to F3 or F4
+  t = 2 if red2 else 3
+  f_sum[t] += time
+  f_cnt[t] += 1
+
+def calculate_options(tmp, mont_m, mont_params):
+  # Presume k = 1
+  tmp_k1, _ = mont_mul(tmp, mont_m, mont_params)
+  tmp_k1, red1 = mont_mul(tmp_k1, tmp_k1, mont_params)
+
+  # Presume k = 0
+  tmp_k0, red2 = mont_mul(tmp, tmp, mont_params)
+
+  return (tmp_k0, tmp_k1)
+  
 def attack():
   N = params[0]
-  some_c = [random.randrange(N) for _ in range(10000)]
+  some_c = [random.randrange(N) for _ in range(4000)]
   found_d = 1
 
   # Get some mont params
@@ -68,11 +121,25 @@ def attack():
   N_bits = mont_params['N_size'] * 64
   final_cutoff = 4.0
 
-  for key_index in xrange(1, N_bits):
+  # Keep note of our failure stage
+  fail_stage_cnt = 0
+
+  # Print the first bit, for completeness' sake
+  print("Found bit 1", bin(found_d))
+
+  key_index = 1
+  while key_index < N_bits:
+    assert key_index >= 1
+
     f_sum = [0.0, 0.0, 0.0, 0.0]
     f_cnt = [ 0,   0,   0,   0 ]
 
-    for challenge_index, tmp in enumerate(mont_tmps):      
+    # Make the key 1 bit longer
+    found_d = found_d << 1
+
+    assert len(bin(found_d)) - 2 == key_index + 1
+
+    for challenge_index, tmp in enumerate(mont_tmps):
       # Presume k = 1
       tmp_k1, _ = mont_mul(tmp, mont_m_list[challenge_index], mont_params)
       tmp_k1, red1 = mont_mul(tmp_k1, tmp_k1, mont_params)
@@ -82,21 +149,7 @@ def attack():
       tmp_k0, red2 = mont_mul(tmp, tmp, mont_params)
       mont_tmps_k0[challenge_index] = tmp_k0
 
-      # Add to F1 (0) if red1, else F2 (1)
-      if red1:
-        f_sum[0] = f_sum[0] + times[challenge_index]
-        f_cnt[0] = f_cnt[0] + 1
-      else:
-        f_sum[1] = f_sum[1] + times[challenge_index]
-        f_cnt[1] = f_cnt[1] + 1
-
-      # Add to F3 or F4
-      if red2:
-        f_sum[2] = f_sum[2] + times[challenge_index]
-        f_cnt[2] = f_cnt[2] + 1
-      else:
-        f_sum[3] = f_sum[3] + times[challenge_index]
-        f_cnt[3] = f_cnt[3] + 1
+      update_averages(times[challenge_index], red1, red2, f_sum, f_cnt)
 
     f_avg = [0.0, 0.0, 0.0, 0.0]
     for i in range(0, 4):
@@ -105,22 +158,64 @@ def attack():
     diff_f1_f2 = f_avg[0] - f_avg[1]
     diff_f3_f4 = f_avg[2] - f_avg[3]
 
-    found_d = found_d << 1
-
+    #print(diff_f1_f2, diff_f3_f4)
+  
     if diff_f1_f2 < final_cutoff and diff_f3_f4 < final_cutoff:
-      print("Guessing target key size of {0} bits".format(key_index + 1))
-      # Return our current value, we must test both values of bit i
-      return (found_d | 1)
+      # We haven't seen a significant difference. Check the fail_stage_cnt:
+      # 0 = First difficulty, check if we are done, add samples if not
+      # 1 = Stuck, go back and flip bit
+
+      # if we are at the first bit, we cannot backtrack so just grow regardless.
+      if fail_stage_cnt == 0 or key_index == 1:
+        print("Guessing target key size of {0} bits".format(key_index + 1))
+
+        if verify_d(found_d):
+          return found_d
+        else:
+          found_d = found_d ^ 1
+          if verify_d(found_d):
+            return found_d
+          else:
+            print('Not done... Adding more samples.')
+            # Blank the lowest bit so challenges added are correct
+            found_d = found_d >> 1
+            found_d = found_d << 1
+            add_challenges(2000, some_c, times, mont_m_list, mont_tmps, found_d, mont_params)
+            print('New size:', len(some_c))
+            # Need to expand the temp lists...
+            mont_tmps_k0.extend([None]*(len(some_c)-len(mont_tmps_k0)))
+            mont_tmps_k1.extend([None]*(len(some_c)-len(mont_tmps_k1)))
+            # Retry this bit, i.e. don't increment key_index, shift back
+            key_index = key_index
+            found_d = found_d >> 1
+            fail_stage_cnt = 1
+      elif fail_stage_cnt == 1:
+        print('Not done... Backtracking to bit:', key_index - 1, 'Erroneous d:', bin(found_d))
+        # Just go back a bit. Our larger sample size should be enough to determine now (hopefully).
+        key_index -= 1
+        found_d = found_d >> 2
+        # We need to update the mont_tmps for this new value.
+        for ind, c in enumerate(some_c):
+          c_pow_d = pow(c, found_d << 1, mont_params['N'])
+          mont_tmps[ind] = get_mont_rep(c_pow_d, mont_params)
     elif diff_f1_f2 > diff_f3_f4:
       # Guess k is hot
       found_d = found_d | 1
+      print("Found bit", key_index + 1, bin(found_d))
       # Keep k1 list
-      mont_tmps = mont_tmps_k1
+      mont_tmps = list(mont_tmps_k1)
+      # Reset fail state
+      fail_stage_cnt = 0
+      key_index += 1
     else:
       # Guess k low
-      mont_tmps = mont_tmps_k0
+      print("Found bit", key_index + 1, bin(found_d))
+      mont_tmps = list(mont_tmps_k0)
+      # Reset fail state
+      fail_stage_cnt = 0
+      key_index += 1
 
-    print("Found bit", key_index, bin(found_d))
+  ## end while
 
   return found_d
 
@@ -156,7 +251,7 @@ if ( __name__ == "__main__" ) :
   # Check our guess
   if not verify_d(guess_d):
     guess_d = guess_d ^ 1
-    assert(verify_d)
+    assert(verify_d(guess_d))
 
   print("Recovered private key d:\n{0:x}".format(guess_d))
 
