2ef959cd3a90e0f1cc7dd4e368119cec9c8a3b89
George Field <gf12815@my.bristol.ac.uk>
2015-05-03 23:24:53 +0200

    Getting closer to imitating matlab. A checkpoint is in order.

--- COMS30901/att/power/attack.py
+++ COMS30901/att/power/attack.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 from __future__ import print_function
 import sys, subprocess, math
+import scipy.io, numpy
 
 def interact(m) :
   global queries
@@ -26,7 +27,57 @@ def interact(m) :
   return (c, trace)
 
 def attack() :
-  (c, trace) = interact(0xabcd)
+#  (c, trace) = interact(0xabcd)
+
+
+  # Try recreating the matlab code
+  ws = scipy.io.loadmat('WS1.mat', squeeze_me=True)
+
+  first = 0
+  last = 255
+
+  SubBytes = ws['SubBytes']
+  byte_Hamming_weight = ws['byte_Hamming_weight']
+  traces = ws['traces']
+
+  b = 1
+  # inputs = ws['inputs'][:,b]
+  inputs = ws['inputs']
+
+  print('Predicting intermediate values ...')
+  m,n = ws['traces'].shape
+
+  key = range(256)
+  after_sbox = numpy.zeros((m,256), dtype=numpy.uint8)
+
+  for i in range(m):
+    xored = inputs[i] ^ key
+    after_sbox[i,:] = SubBytes[xored]
+
+  key_trace = numpy.zeros((256,n))
+
+  # correlation method
+  print('Predicting the instantaneous power consumption ...')
+  power_consumption = byte_Hamming_weight[after_sbox]
+
+  print('Generating the correlation traces ...')
+
+  chunksize = 50
+  chunks = n / 50
+  #for i in range(1):
+    #for j in range(1,2):
+  for i in range(first, last+1):
+    for j in range(1,chunks):
+      #print(i, j)
+      ccarg = numpy.column_stack((traces[:,(j-1)*chunksize:j*chunksize], power_consumption[:,i]))
+      cmatrix = numpy.corrcoef(ccarg, rowvar=0)
+      #print(numpy.mean(ccarg), numpy.mean(cmatrix), ccarg.shape, cmatrix.shape, cmatrix[7,5], cmatrix[32,12])
+      key_trace[i,(j-1)*chunksize:j*chunksize+1] = cmatrix[chunksize,0:chunksize+1];
+      #print(key_trace[i,(j-1)*chunksize:j*chunksize])
+      #print(key_trace.shape, numpy.mean(key_trace))
+
+  global kt
+  kt = key_trace
 
 def launch_target(executable) :
   # Produce a sub-process representing the attack target.
