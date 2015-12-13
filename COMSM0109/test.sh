dc8baa3c73c62c1ff466f82e2fcf85065bae5ffa
George Field <gf12815@my.bristol.ac.uk>
2015-12-13 19:15:36 +0000

    Remove silly bsort and add testing script.

---
+++ COMSM0109/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+all_tests=$(fgrep -rl ';;;test;;;' *.ass)
+passed=()
+failed=()
+for t in $all_tests; do
+    echo $t
+    ./assembler.py < $t
+    if yes 'c 50' | ./sim.py out.bin out_test.py > /dev/null; then
+	passed+=("$t")
+    else
+	failed+=("$t")
+    fi
+done
+echo "Passed ${#passed[*]}: ${passed[*]}"
+echo "Failed ${#failed[*]}: ${failed[*]}"
