875cd9be01fe8b1dad6c9eedd96c8dc64abc0cc0
George Field <gf12815@my.bristol.ac.uk>
2015-12-17 22:58:46 +0000

    Extend dynamic predictor to support 2 bits.

--- COMSM0109/BranchPredictor.py
+++ COMSM0109/BranchPredictor.py
@@ -19,10 +19,14 @@ class StaticPredictor(object):
 class DynamicPredictor(object):
     """ A dynamic branch predictor, with a 1-bit counter per branch. """
 
-    def __init__(self):
+    def __init__(self, bits, default):
         self._table = {}
         self._dbg_log = []
-        self.DEFAULT = 1
+        self.BITS = bits
+        self.DEFAULT = default
+
+        self._ctrlim = 2 ** bits
+        self._decbound = self._ctrlim / 2
 
     def _getOrInit(self, ins):
         if ins.asrc not in self._table:
@@ -32,9 +36,12 @@ class DynamicPredictor(object):
     def predict(self, ins):
         """ Return true if predict taken. """
         state = self._getOrInit(ins)
-        return state == 1
+        return state >= self._decbound
 
     def branchResult(self, ins, taken):
         """ Update state depending on outcome. """
         self._dbg_log.append((ins,taken))
-        self._table[ins.asrc] = 1 if taken else 0
+        if taken:
+            self._table[ins.asrc] = min(self._ctrlim - 1, self._table[ins.asrc] + 1)
+        else:
+            self._table[ins.asrc] = max(0, self._table[ins.asrc] - 1)
