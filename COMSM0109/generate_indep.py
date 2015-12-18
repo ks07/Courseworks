2d35b21138f06117a78803ac7be3fc6abe964e9b
George Field <gf12815@my.bristol.ac.uk>
2015-12-18 15:48:30 +0000

    Add dependent sequence experiment.

--- COMSM0109/generate_indep.py
+++ COMSM0109/generate_indep.py
@@ -2,10 +2,10 @@
 
 import random
 
-with open('gen.ass', 'w') as f:
-    inlist = ['    addi r{0:d},{1:d}\n'.format(i % 32, random.randint(0, 0xFFFF)) for i in range(256)]
-    inlist.append('    halt')
-    f.writelines(inlist)
+# with open('gen.ass', 'w') as f:
+#     inlist = ['    addi r{0:d},{1:d}\n'.format(i % 32, random.randint(0, 0xFFFF)) for i in range(256)]
+#     inlist.append('    halt')
+#     f.writelines(inlist)
 
 
 # inlist = []
@@ -22,3 +22,8 @@ with open('gen.ass', 'w') as f:
 #     inlist = ['    movi r{0:d},{1:d}\n'.format(1, random.randint(0, 0xFFFF)) for i in range(256)]
 #     inlist.append('    halt')
 #     f.writelines(inlist)
+
+with open('gen.ass', 'w') as f:
+    inlist = ['    addi r{0:d},{1:d}\n'.format(1, random.randint(0, 0xFFFF)) for i in range(256)]
+    inlist.append('    halt')
+    f.writelines(inlist)
