03134116a62db4f9c1bd219094269c86343910d8
George Field <gf12815@my.bristol.ac.uk>
2015-12-22 17:03:51 +0000

    Try to turn back when cloud gets bigger.

--- COMSM4111/Controller.m
+++ COMSM4111/Controller.m
@@ -8,16 +8,25 @@ classdef Controller < handle
         prevGPS;    % The previously recorded GPS position.
         prevPPM;    % The previously recorded PPM.
         state;
-        POS_BOUND = 800;%1000;	% Max distance in any direction.
-        PPM_BOUND = 1;  % PPM that signifies the desired boundary.
+        
+        inside_measures; % Stores last 4 measurements in inside state.
+        state_ctr;
+        
+        PPM_UPPER;
+        PPM_LOWER;
     end
     
     properties (Constant)
-      STATE_LOST = 0;
-      STATE_INCREASING = 1;
-      STATE_FOUND = 2;
-      STATE_LEAVING = 3;
-      STATE_RETURNING = 4;
+        STATE_LOST = 0; % Not in cloud
+        STATE_INCREASING = 1; % Detected moving towards cloud
+        STATE_FOUND = 2; % At approx 1ppm boundary
+        STATE_LEAVING = 3; % Leaving map bounds
+        STATE_RETURNING = 4; % Just made a turn after leaving
+        STATE_INSIDE = 5; % Too far into cloud
+        
+        
+        POS_BOUND = 800;%1000;	% Max distance in any direction.
+        PPM_BOUND = 1;  % PPM that signifies the desired boundary.
     end
     
     methods
@@ -28,6 +37,11 @@ classdef Controller < handle
             ctrl.prevGPS = [0 0];
             ctrl.prevPPM = 0;
             ctrl.state = ctrl.STATE_LOST;
+            ctrl.state_ctr = 0;
+            
+            ppm_pcnt = 0.1;
+            ctrl.PPM_LOWER = ctrl.PPM_BOUND * (1 - ppm_pcnt);
+            ctrl.PPM_UPPER = ctrl.PPM_BOUND * (1 + ppm_pcnt);
         end
         function step(self,t)
             [gps, ppm] = self.uav.getInput(self.cloud,t);
@@ -35,8 +49,24 @@ classdef Controller < handle
             self.estimateHeading(gps);
             
             % State transition function(s)
-            if ppm >= self.PPM_BOUND
+            if self.state == self.STATE_INSIDE
+                
+            elseif self.state == self.STATE_FOUND && self.state_ctr < 4
+                disp('counting up');
+                self.state_ctr = self.state_ctr + 1;
+                self.inside_measures = [self.inside_measures ppm];
+            elseif ppm >= self.PPM_LOWER && ppm <= self.PPM_UPPER
+                disp('in bounds')
+                self.state_ctr = 0;
                 self.state = self.STATE_FOUND;
+                self.inside_measures = ppm;
+            elseif self.state == self.STATE_FOUND
+                avg_ppm = mean(self.inside_measures)
+                if avg_ppm > self.PPM_UPPER
+                    self.state = self.STATE_INSIDE;
+                end
+            elseif ppm > self.PPM_UPPER
+                self.state = self.STATE_INSIDE;
             elseif self.state == self.STATE_LEAVING
                 self.state = self.STATE_RETURNING;
             elseif ~self.checkBounds(gps)
@@ -69,6 +99,13 @@ classdef Controller < handle
                     disp('moving away from boundary');
                     self.uav.cmdTurn(-0.1);
                     self.uav.cmdSpeed(20);
+                case self.STATE_INSIDE
+                    disp('too far inside');
+                    if self.prevPPM > ppm
+                        self.uav.cmdTurn(0);
+                        self.uav.cmdSpeed(10);
+                        self.state = self.STATE_LOST;
+                    end
             end
             
             self.uav.updateState(self.dt);
@@ -89,7 +126,7 @@ classdef Controller < handle
             a = a / norm(a);
             b = b / norm(b);
             %estHDG = atan2(norm(cross(a,b)), dot(a,b))
-            estHDG = acosd(a(:).'*b(:))
+            estHDG = acosd(a(:).'*b(:));
         end
     end
     
