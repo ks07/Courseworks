d11cb53d201bd0175709f0de465da0e9a55ab24b
George Field <gfield145@gmail.com>
2016-01-25 11:38:11 +0000

    Try to spin and pick best direction to stay inside bounds, but sometimes this means edging along parallel to the border...

--- COMSM4111/StateInterruptLeaving.m
+++ COMSM4111/StateInterruptLeaving.m
@@ -1,31 +1,50 @@
-classdef StateInterruptLeaving
+classdef StateInterruptLeaving < handle
     %STATEINTERRUPTLEAVING Interrupt state, we are dangerously close to the
     % range limit.
     %   Detailed explanation goes here
     
     properties
+        ctr;
+        points;
     end
     
     properties(Constant)
         TRIGGER_POS_BOUND = 800; % Min distance in any direction to trigger
+        STEPS = 5;
     end
     
     methods
         function state = StateInterruptLeaving()
+            state.ctr = 1;
+            state.points = zeros(state.STEPS,1);
         end
-        function newState = step(~, ~, c)
+        function newState = step(state, t, c)
             % About to go out of range, turn!
-            c.uav.cmdTurn(-1);
-            c.uav.cmdSpeed(20);
+            newState = state;
+            [gps,~] = c.getInput(t);
+            [spd,trn] = c.calcTurn(360/state.STEPS);
+            if state.ctr <= state.STEPS
+                % Note positions and make a move.
+                state.points(state.ctr) = pdist([gps;0 0]) - pdist([c.prevGPS;0 0]); % Store the diff in dist to origin
+                
+                c.uav.cmdTurn(trn);
+                c.uav.cmdSpeed(spd);
+                
+                c.uav.updateState(c.dt);
+                
+                state.ctr = state.ctr + 1;
+            else
+                % Decision making time.
+                [~,bestend] = min(state.points);
 
-            c.uav.updateState(c.dt);
-
-            newState = StateReturning(); % Don't need to step this, wait one
+                newState = StateReturning(bestend,trn,spd,state.STEPS);
+                newState = newState.step(t,c);
+            end
         end
     end
     methods(Static)
         function interruptState = triggered(state,t,c,gps,ppm)
-            if ~isa(state,'StateReturning') && max(abs(gps)) > StateInterruptLeaving.TRIGGER_POS_BOUND
+            if ~isa(state,'StateReturning') && ~isa(state,'StateInterruptLeaving') && max(abs(gps)) > StateInterruptLeaving.TRIGGER_POS_BOUND
                 interruptState = StateInterruptLeaving();
             else
                 % Not triggered
--- COMSM4111/StateReturning.m
+++ COMSM4111/StateReturning.m
@@ -1,22 +1,41 @@
-classdef StateReturning
+classdef StateReturning < handle
     %STATERETURNING Interrupt state, moving away from the range
     % limit.
     %   Detailed explanation goes here
     
     properties
+        endpick;
+        trn;
+        spd;
+        ctr;
     end
     
     methods
-        function state = StateReturning()
+        function state = StateReturning(bestend,trn,spd,steps)
+            state.endpick = mod(bestend-2,steps)+1;
+            state.trn = trn;
+            state.spd = spd;
+            state.ctr = 1;
         end
-        function newState = step(~, ~, c)
-            % Hopefully moving away from the boundary.
-            c.uav.cmdTurn(-0.1);
-            c.uav.cmdSpeed(20);
-
-            c.uav.updateState(c.dt);
-
-            newState = StateLost(); % Most likely we will trigger leaving again.
+        function newState = step(state, ~, c)
+            if state.ctr < state.endpick
+                % More turns to take.
+                c.uav.cmdTurn(state.trn);
+                c.uav.cmdSpeed(state.spd);
+                
+                c.uav.updateState(c.dt);
+                
+                state.ctr = state.ctr + 1;
+                newState = state;
+            else
+                % Forward once, then resume search.
+                c.uav.cmdTurn(0);
+                c.uav.cmdSpeed(10);
+                
+                c.uav.updateState(c.dt);
+                
+                newState = StateLost();
+            end
         end
     end
     
